2b152b7200abe0c15c4115264ffd03e60f65000f
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-01-04 22:15:23 -0500

    Adding brute force-code for Sumof OddLength

---
+++ Practice/Arrays/SumOfOddLength/CodeFile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Arrays.SumOfOddLength
+{
+    public class CodeFile
+    {
+        public int SumOddLengthSubarrays(int[] arr)
+        {
+            // Brute force
+            var result = 0;
+            var oddSum = 0;
+            var count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                result += arr[i];
+                for (int j = i; j < arr.Length; j++)
+                {
+                    oddSum += arr[j];
+                    count++;
+                    if(count>1 && count%2!=0)
+                    {
+                        result += oddSum;
+                    }
+                }
+
+                count = 0;
+                oddSum = 0;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Practice/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Practice.Arrays.SumOfOddLength;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CodeFile ob = new CodeFile();
+            var input = new int[] { 10,11,12 };
+            var output = ob.SumOddLengthSubarrays(input);
+            Console.WriteLine(output);
+        }
+    }
+}
