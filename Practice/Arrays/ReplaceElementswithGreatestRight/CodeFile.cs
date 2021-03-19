2b678f4964000fe1707a27d55e9b54b6f4bea247
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-03-18 20:20:27 -0400

    Adding code for Replacing elements with right side

---
+++ Practice/Arrays/ReplaceElementswithGreatestRight/CodeFile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Arrays.ReplaceElementswithGreatestRight
+{
+    public class CodeFile
+    {
+        // Brute Force
+        public int[] ReplaceElements(int[] arr)
+        {
+            int largest = 0;
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                largest = arr[i + 1];
+                for (int j = i + 1; j < arr.Length - 1; j++)
+                {
+                    if (largest < arr[j + 1])
+                    {
+                        largest = arr[j + 1];
+                    }
+                }
+
+                arr[i] = largest;
+            }
+
+
+            arr[arr.Length - 1] = -1;
+
+            return arr;
+        }
+    }
+}
