279a7538296d8ac68690de9c030242b8818c4e3f
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-03-29 21:44:29 -0400

    Added second apporach for intersection of linked list

--- Practice/LinkedList/Intersectionof2LinkedList/CodeFile.cs
+++ Practice/LinkedList/Intersectionof2LinkedList/CodeFile.cs
@@ -6,7 +6,7 @@ namespace Practice.LinkedList.Intersectionof2LinkedList
 {
     public class CodeFile
     {
-        public ListNode getIntersectionNode(ListNode headA, ListNode headB)
+        public ListNode getIntersectionNode1(ListNode headA, ListNode headB)
         {
             ListNode pA = headA;
             ListNode pB = headB;
@@ -17,6 +17,53 @@ namespace Practice.LinkedList.Intersectionof2LinkedList
             }
 
             return pA;
+        }
+
+        public ListNode getIntersectionNode(ListNode headA, ListNode headB)
+        {
+            var count1 = 0;
+            var count2 = 0;
+            var first = headA;
+            var second = headB;
+
+            while (first != null)
+            {
+                count1++;
+                first = first.next;
+            }
+
+            while (second != null)
+            {
+                count2++;
+                second = second.next;
+            }
+
+            if (count1 < count2)
+            {
+                return GetResult(headB, headA, count2, count1);
+            }
+            else
+            {
+                return GetResult(headA, headB, count1, count2);
+            }
+        }
+
+        public ListNode GetResult(ListNode nodeA, ListNode nodeB, int count1, int count2)
+        {
+            var difference = count1 - count2;
+            var count = 0;
+            while (nodeA != nodeB)
+            {
+                if (count >= difference)
+                {
+                    nodeB = nodeB.next;
+                }
+
+                nodeA = nodeA.next;
+                count++;
+            }
+
+            return nodeA;
         }
     }
 }
