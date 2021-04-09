f1e538856e5d1ac32265725a0b7aed5bbaf72229
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-04-08 21:51:27 -0400

    Add code for OddEven and Palindrome linked list

---
+++ Practice/LinkedList/OddEvenLinkedList/CodeFile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.LinkedList.OddEvenLinkedList
+{
+    public class CodeFile
+    {
+        public ListNode OddEvenList(ListNode head)
+        {
+            if (head == null) return null;
+
+            var oddHead = head;
+            var evenHead = head.next;
+
+            var odd = head;
+            var even = evenHead;
+
+            while (even != null && even.next != null)
+            {
+                odd.next = even.next;
+                odd = odd.next;
+
+                even.next = odd.next;
+                even = even.next;
+            }
+
+            odd.next = evenHead;
+
+            return oddHead;
+        }
+    }
+}
---
+++ Practice/LinkedList/PalindromeLinkedList/CodeFile.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.LinkedList.PalindromeLinkedList
+{
+    public class CodeFile
+    {
+        public bool IsPalindrome(ListNode head)
+        {
+            if (head == null) return true;
+
+            var p1 = head;
+            var endOfFirstHalf = EndOfFirstHalf(head);
+
+            var p2 = ReverseLinkedList(endOfFirstHalf.next);
+            while (p2 != null)
+            {
+                if (p1.val != p2.val)
+                {
+                    return false;
+                }
+
+                p1 = p1.next;
+                p2 = p2.next;
+            }
+
+            return true;
+        }
+
+        private ListNode EndOfFirstHalf(ListNode head)
+        {
+            var fast = head;
+            var slow = head;
+
+            while (fast.next != null && fast.next.next != null)
+            {
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+
+            return slow;
+        }
+
+        private ListNode ReverseLinkedList(ListNode head)
+        {
+            ListNode previous = null;
+            ListNode current = head;
+
+            while (current != null)
+            {
+                var nextNode = current.next;
+                current.next = previous;
+                previous = current;
+                current = nextNode;
+            }
+
+            return previous;
+        }
+    }
+}
