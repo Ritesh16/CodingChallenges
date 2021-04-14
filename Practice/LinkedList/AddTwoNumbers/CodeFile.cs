f7689c5fea1fcd4a6fc08c67bc1b0a0fa6c10d3f
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-04-13 21:51:52 -0400

    Adding code for Add 2 numbers and merging sorted list

---
+++ Practice/LinkedList/AddTwoNumbers/CodeFile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.LinkedList.AddTwoNumbers
+{
+    public class CodeFile
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            var prehead = new ListNode(0);
+            //prehead.next = l1;
+
+            var p1 = l1;
+            var p2 = l2;
+            var carry = 0;
+            var curr = prehead;
+
+            while (p1 != null || p2 != null)
+            {
+                var x = p1 != null ? p1.val : 0;
+                var y = p2 != null ? p2.val : 0;
+
+                var sum = x + y + carry;
+
+                carry = sum / 10;
+                curr.next = new ListNode(sum % 10);
+                curr = curr.next;
+                if (p1 != null) p1 = p1.next;
+
+                if (p2 != null) p2 = p2.next;
+            }
+
+            if (carry != 0)
+            {
+                curr.next = new ListNode(carry);
+            }
+
+            return prehead.next;
+        }
+    }
+}
