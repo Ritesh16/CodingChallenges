3045851c03024341253f9e5711319c2c08d8fd63
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-05-06 14:53:49 -0400

    Added code for moving average

---
+++ Practice/StacksAndQueues/MovingAverage/CodeFile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.StacksAndQueues.MovingAverage
+{
+    public class MovingAverage1
+    {
+
+        Queue<int> queue;
+        int cap;
+        public MovingAverage1(int size)
+        {
+            cap = size;
+            queue = new Queue<int>(size);
+        }
+
+        public double Next(int val)
+        {
+            if (queue.Count == cap)
+            {
+                queue.Dequeue();
+            }
+
+            queue.Enqueue(val);
+            var sum = 0;
+            foreach (var item in queue)
+            {
+                sum += item;
+            }
+
+            double s = (double)sum / queue.Count;
+            return s;
+        }
+    }
+}
---
+++ Practice/StacksAndQueues/MovingAverage/CodeFile2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.StacksAndQueues.MovingAverage
+{
+    public class MovingAverage
+    {
+        int[] queue;
+        int size;
+        int tail;
+        int head;
+        int count;
+        int sum;
+        public MovingAverage(int size)
+        {
+            queue = new int[size];
+            this.size = size;
+            head = 0;
+            tail = 0;
+            count = 0;
+            sum = 0;
+        }
+
+        public double Next(int val)
+        {
+            count++;
+            tail = (head + 1) % size;
+
+            sum = sum - queue[tail] + val;
+
+            head = (head + 1) % size;
+            queue[head] = val;
+
+            return sum * 1.0 / Math.Min(count, size);
+        }
+    }
+}
