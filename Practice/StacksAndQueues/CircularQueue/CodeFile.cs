193b8396b437298e70891f416be4cf2092df92fd
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-05-05 21:30:21 -0400

    Adding code for circular queue

---
+++ Practice/StacksAndQueues/CircularQueue/CodeFile.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.StacksAndQueues.CircularQueue
+{
+    public class MyCircularQueue
+    {
+
+        int[] array;
+        int length;
+        int head = -1;
+        int tail = -1;
+        public MyCircularQueue(int k)
+        {
+            array = new int[k];
+            length = k;
+        }
+
+        public bool EnQueue(int value)
+        {
+            if (IsFull())
+            {
+                return false;
+            }
+
+            var result = false;
+            if (head == -1)
+            {
+                head = 0;
+            }
+
+            if (head > 0 && tail == length - 1) tail = -1;
+
+            if (tail < length - 1)
+            {
+                tail++;
+                array[tail] = value;
+                result = true;
+            }
+
+            return result;
+        }
+
+        public bool DeQueue()
+        {
+            var result = false;
+            if (head == -1)
+            {
+                return result;
+            }
+
+            if (head == tail)
+            {
+                array[head] = 0;
+                head = -1;
+                tail = -1;
+                return true;
+            }
+
+            array[head] = -1;
+            head++;
+
+            if (head == length && head != tail)
+            {
+                head = 0;
+            }
+
+            result = true;
+            return result;
+        }
+
+        public int Front()
+        {
+            if (head == -1) return -1;
+            return array[head];
+        }
+
+        public int Rear()
+        {
+            if (tail == -1) return -1;
+            return array[tail];
+        }
+
+        public bool IsEmpty()
+        {
+            var result = false;
+            if (tail == -1 && head == -1) result = true;
+
+            return result;
+        }
+
+        public bool IsFull()
+        {
+            var result = false;
+            if (head == 0 && tail == length - 1)
+            {
+                result = true;
+            }
+
+            if (head - 1 == tail)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+    }
+
+}
---
+++ Practice/StacksAndQueues/CircularQueue/ImprovedCode.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.StacksAndQueues.CircularQueue
+{
+    public class MyCircularQueue_I
+    {
+        int[] array;
+        int length;
+        int head = 0;
+        int tail = 0;
+        public MyCircularQueue_I(int k)
+        {
+            array = new int[k];
+            length = 0;
+            head = 0;
+            tail = -1;
+        }
+
+        public bool EnQueue(int value)
+        {
+            if (IsFull())
+            {
+                return false;
+            }
+
+            tail = (tail + 1) % array.Length;
+            array[tail] = value;
+            length++;
+            return true;
+        }
+
+        public bool DeQueue()
+        {
+            if (IsEmpty())
+            {
+                return false;
+            }
+
+            head = (head + 1) % array.Length;
+            length--;
+
+            return true;
+        }
+
+        public int Front()
+        {
+            return IsEmpty() ? -1 : array[head];
+        }
+
+        public int Rear()
+        {
+            return IsEmpty() ? -1 : array[tail];
+        }
+
+        public bool IsEmpty()
+        {
+            return length == 0;
+        }
+
+        public bool IsFull()
+        {
+            return length == array.Length;
+        }
+    }
+}
