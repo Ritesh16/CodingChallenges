df77678f5ae8283ece339f0970ee88493590367a
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-05-11 20:53:49 -0400

    Add MinStack and PerfectSquares

---
+++ Practice/StacksAndQueues/MinStack/CodeFile.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.StacksAndQueues.MinStack
+{
+    public class MinStack
+    {
+        /** initialize your data structure here. */
+        Stack<int> stack;
+        Stack<int[]> minStack;
+        public MinStack()
+        {
+            stack = new Stack<int>();
+            minStack = new Stack<int[]>();
+        }
+
+        public void Push(int val)
+        {
+            stack.Push(val);
+
+            if (minStack.Count == 0 || minStack.Peek()[0] > val)
+            {
+                var array = new int[] { val, 1 };
+                minStack.Push(array);
+            }
+            else if (minStack.Peek()[0] == val)
+            {
+                minStack.Peek()[1]++;
+            }
+
+        }
+
+        public void Pop()
+        {
+            var top = stack.Peek();
+            var minTop = minStack.Peek();
+
+            if (top == minTop[0])
+            {
+                minTop[1]--;
+                if (minTop[1] == 0)
+                {
+                    minStack.Pop();
+                }
+            }
+
+            stack.Pop();
+        }
+
+        public int Top()
+        {
+            return stack.Peek();
+        }
+
+        public int GetMin()
+        {
+            return minStack.Peek()[0];
+        }
+    }
+}
---
+++ Practice/StacksAndQueues/PerfectSquares/CodeFile.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.StacksAndQueues.PerfectSquares
+{
+    public class CodeFile
+    {
+        public int NumSquares(int n)
+        {
+            if (n < 4)
+            {
+                return n;
+            }
+
+            var squares = new List<int>();
+            for (int i = 1; i < n; i++)
+            {
+                squares.Add(i * i);
+            }
+
+            int level = 0;
+            var queue = new HashSet<int>();
+            queue.Add(n);
+
+            while (queue.Count > 0)
+            {
+                level++;
+                var newQueue = new HashSet<int>();
+                foreach (var number in queue)
+                {
+                    foreach (var square in squares)
+                    {
+                        if (number == square)
+                        {
+                            return level;
+                        }
+                        else if (number < square)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            newQueue.Add(number - square);
+                        }
+                    }
+                }
+
+                queue = newQueue;
+            }
+
+            return level;
+        }
+    }
+}
