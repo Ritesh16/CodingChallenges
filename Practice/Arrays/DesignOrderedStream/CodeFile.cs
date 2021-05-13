58591f3e1ea06f63a708fee4f7bd59da7362a586
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-05-12 21:29:51 -0400

    Adding code for arrays-DesignOrderedStream &7 MinTimeVisitingAllPoints

---
+++ Practice/Arrays/DesignOrderedStream/CodeFile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Arrays.DesignOrderedStream
+{
+    public class CodeFile
+    {
+        string[] array;
+        int pointer;
+        public CodeFile(int n)
+        {
+            pointer = 1;
+            array = new string[n + 1];
+        }
+
+        public IList<string> Insert(int idKey, string value)
+        {
+            array[idKey] = value;
+            List<string> output = new List<string>();
+            if (pointer == idKey)
+            {
+                int i = idKey;
+                for (; i < array.Length; i++)
+                {
+                    if (array[i] != null)
+                    {
+                        output.Add(array[i]);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                pointer = i;
+            }
+
+            return output;
+        }
+    }
+}
---
+++ Practice/Arrays/MinTimeVisitingAllPoints/CodeFile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Arrays.MinTimeVisitingAllPoints
+{
+    public class CodeFile
+    {
+        public int MinTimeToVisitAllPoints(int[][] points)
+        {
+            int time = 0;
+            for (int i = 1; i < points.Length; i++)
+            {
+                var a = points[i - 1];
+                var b = points[i];
+                int dx = Math.Abs(a[0] - b[0]);
+                int dy = Math.Abs(a[1] - b[1]);
+                time += Math.Max(dx, dy);
+            }
+            return time;
+        }
+    }
+}
