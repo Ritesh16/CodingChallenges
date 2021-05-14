f71feab379f3d983f0c7c7bd08fd89d4d8639e03
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-05-13 20:55:45 -0400

    Adding problems for stack : DailyTemperature & EvaluateReversePolishNotation

---
+++ Practice/StacksAndQueues/DailyTemperatures/CodeFile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.StacksAndQueues.DailyTemperatures
+{
+    public class CodeFile
+    {
+        public int[] DailyTemperatures(int[] temperatures)
+        {
+            var stack = new Stack<int>();
+            var result = new int[temperatures.Length];
+
+            for (int i = 0; i < temperatures.Length; i++)
+            {
+                while (stack.Count > 0 && temperatures[stack.Peek()] < temperatures[i])
+                {
+                    var index = stack.Pop();
+                    result[index] = i - index;
+                }
+
+                stack.Push(i);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Practice/StacksAndQueues/EvaluateReversePolishNotation/CodeFile.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.StacksAndQueues.EvaluateReversePolishNotation
+{
+    public class CodeFile
+    {
+        public int EvalRPN(string[] tokens)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                if (tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/")
+                {
+                    var num1 = stack.Pop();
+                    var num2 = stack.Pop();
+
+                    var cal = Calculate(num2, num1, tokens[i]);
+                    stack.Push(cal);
+                }
+                else
+                {
+                    stack.Push(Convert.ToInt32(tokens[i]));
+                }
+            }
+
+            var result = stack.Pop();
+            return result;
+        }
+        private int Calculate(int num1, int num2, string op)
+        {
+            int result = 0;
+            switch (op)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+
+                case "-":
+                    result = num1 - num2;
+                    break;
+
+                case "*":
+                    result = num1 * num2;
+                    break;
+
+                case "/":
+                    result = num1 / num2;
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
