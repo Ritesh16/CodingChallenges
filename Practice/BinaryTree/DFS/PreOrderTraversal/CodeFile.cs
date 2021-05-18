ea2e6f5af30b86d17b55fba42472fd72ded29661
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-05-17 21:47:15 -0400

    Added code for Preorder traversal

---
+++ Practice/BinaryTree/DFS/PreOrderTraversal/CodeFile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.BinaryTree.DFS.PreOrderTraversal
+{
+    public class CodeFile
+    {
+        public IList<int> PreorderTraversal(TreeNode root)
+        {
+            var output = new List<int>();
+            var stack = new Stack<TreeNode>();
+            if (root == null)
+            {
+                return output;
+            }
+
+            stack.Push(root);
+
+            while (stack.Count > 0)
+            {
+                var node = stack.Pop();
+                if (node.right != null)
+                {
+                    stack.Push(node.right);
+                }
+
+                if (node.left != null)
+                {
+                    stack.Push(node.left);
+                }
+
+                output.Add(node.val);
+            }
+
+            return output;
+        }
+    }
+}
