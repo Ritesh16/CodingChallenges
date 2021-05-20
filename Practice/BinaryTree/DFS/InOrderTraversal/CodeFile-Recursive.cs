cb5f0f1da4b832671d0c0f412a6dba3e25831990
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-05-19 22:23:36 -0400

    Adding inorder and post order

---
+++ Practice/BinaryTree/DFS/InOrderTraversal/CodeFile-Recursive.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.BinaryTree.DFS.InOrderTraversal
+{
+    public class CodeFile_Recursive
+    {
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            var output = new List<int>();
+            InOrder_Recursive(root, output);
+            return output;
+        }
+
+        private void InOrder_Recursive(TreeNode root, List<int> output)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            InOrder_Recursive(root.left, output);
+            if (root != null)
+            {
+                output.Add(root.val);
+            }
+
+            InOrder_Recursive(root.right, output);
+        }
+    }
+}
---
+++ Practice/BinaryTree/DFS/PostOrderTraversal/CodeFile-Recursive.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.BinaryTree.DFS.PostOrderTraversal
+{
+    public class CodeFile_Recursive
+    {
+        public IList<int> PostorderTraversal(TreeNode root)
+        {
+            var output = new List<int>();
+            Postorder_Recursive(root, output);
+            return output;
+        }
+
+        private void Postorder_Recursive(TreeNode root, List<int> output)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            Postorder_Recursive(root.left, output);
+            Postorder_Recursive(root.right, output);
+            if (root != null)
+            {
+                output.Add(root.val);
+            }
+        }
+    }
+}
