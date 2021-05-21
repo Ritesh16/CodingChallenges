998c0bbf6fd80c61652c7d898be5e731e10e3347
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-05-20 22:19:42 -0400

    Adding code for array problem

---
+++ Practice/Arrays/MinOptoMakeArrayIncreasing/CodeFile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Arrays.MinOptoMakeArrayIncreasing
+{
+    public class CodeFile
+    {
+        public int MinOperations(int[] nums)
+        {
+            int operations = 0;
+            if (nums.Length < 1)
+            {
+                return operations;
+            }
+
+            int i = 1;
+            while (i < nums.Length)
+            {
+                var prev = i - 1;
+                if (nums[prev] < nums[i])
+                {
+                    i++;
+                }
+                else
+                {
+                    var difference = nums[prev] - nums[i] + 1;
+                    nums[i] = nums[i] + difference;
+                    operations += difference;
+                }
+            }
+
+
+            return operations;
+        }
+    }
+}
