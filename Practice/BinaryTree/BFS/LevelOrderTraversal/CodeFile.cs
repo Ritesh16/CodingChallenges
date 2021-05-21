09034c5dc014222465adf4089df11de27cc4f5ef
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-05-20 22:17:21 -0400

    Adding code for LevelOrder Traversal BFS

---
+++ Practice/BinaryTree/BFS/LevelOrderTraversal/CodeFile.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.BinaryTree.BFS.LevelOrderTraversal
+{
+    public class CodeFile
+    {
+        public IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            var output = new List<IList<int>>();
+
+            if (root == null)
+            {
+                return output;
+            }
+
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            var list = new List<int>();
+            list.Add(root.val);
+            output.Add(list);
+            while (queue.Count > 0)
+            {
+                list = new List<int>();
+                var level = queue.Count;
+                for (int i = 0; i < level; i++)
+                {
+                    var node = queue.Dequeue();
+
+                    if (node.left != null)
+                    {
+                        queue.Enqueue(node.left);
+                        list.Add(node.left.val);
+                    }
+
+                    if (node.right != null)
+                    {
+                        queue.Enqueue(node.right);
+                        list.Add(node.right.val);
+                    }
+                }
+
+                if (list.Count > 0)
+                    output.Add(list);
+            }
+
+            return output;
+        }
+    }
+}
---
+++ Practice/BinaryTree/BFS/LevelOrderTraversal/CodeFileRecursive.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.BinaryTree.BFS.LevelOrderTraversal
+{
+    public class CodeFileRecursive
+    {
+        public IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            var output = new List<IList<int>>();
+            Helper(root, output, 0);
+            return output;
+        }
+
+        public void Helper(TreeNode root, IList<IList<int>> output, int level)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            if (output.Count > level)
+            {
+                var list = output[level];
+                list.Add(root.val);
+            }
+            else
+            {
+                var list = new List<int>();
+                list.Add(root.val);
+                output.Add(list);
+            }
+
+            level++;
+            Helper(root.left, output, level);
+            Helper(root.right, output, level);
+        }
+    }
+}
