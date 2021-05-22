91a787a44c3a30eb6e6e78b6970c7c514895f6bd
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-05-21 22:35:32 -0400

    Added code for string and sql

---
+++ Practice/SQL/FindTeamSize/Query.sql
@@ -0,0 +1,7 @@
+﻿/* Write your T-SQL query statement below */
+SELECT employee_id, T.SIZE AS team_size
+FROM Employee AS E
+LEFT JOIN
+(SELECT TEAM_ID, COUNT(TEAM_ID) AS SIZE FROM Employee GROUP BY TEAM_ID) AS T 
+ON E.team_id = T.TEAM_ID
+GROUP BY employee_id,T.SIZE;
---
+++ Practice/Strings/RemoveVowels/CodeFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Strings.RemoveVowels
+{
+    public class CodeFile
+    {
+        public string RemoveVowels(string s)
+        {
+            return s.Replace("a", "").Replace("e", "").Replace("i", "").Replace("o", "").Replace("u", "");
+        }
+    }
+}
