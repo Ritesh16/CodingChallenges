ca9801f7a858b637d5c32e7fc0ff633d4220eba2
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-05-24 21:39:45 -0400

    Adding code for String problems

---
+++ Practice/Strings/DefangingIPAddress/CodeFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Strings.DefangingIPAddress
+{
+    public class CodeFile
+    {
+        public string DefangIPaddr(string address)
+        {
+            if (!string.IsNullOrEmpty(address))
+            {
+                return address.Replace(".", "[.]");
+            }
+
+            return address;
+        }
+    }
+}
---
+++ Practice/Strings/Single-RowKeyboard/CodeFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Strings.Single_RowKeyboard
+{
+    public class CodeFile
+    {
+        public int CalculateTime(string keyboard, string word)
+        {
+            var dictionary = new Dictionary<char, int>();
+            for (int i = 0; i < keyboard.Length; i++)
+            {
+                dictionary.Add(keyboard[i], i);
+            }
+
+            int result = 0;
+            int last = 0;
+            int temp = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                var c = word[i];
+                temp = Math.Abs(dictionary[c] - last);
+                result = result + temp;
+                last = dictionary[c];
+            }
+
+            return result;
+        }
+    }
+}
