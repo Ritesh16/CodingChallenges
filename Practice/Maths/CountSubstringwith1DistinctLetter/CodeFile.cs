a099a8ad41d4b8ac76ada637095d1ba97856317a
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-05-26 22:06:30 -0400

    Adding math problems

---
+++ Practice/Maths/CountSubstringwith1DistinctLetter/CodeFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Maths.CountSubstringwith1DistinctLetter
+{
+    class CodeFile
+    {
+        public int CountLetters(string s)
+        {
+            var result = 1;
+            var total = 1;
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i] == s[i - 1])
+                {
+                    result += 1;
+                }
+                else
+                {
+                    result = 1;
+                }
+
+                total += result;
+            }
+
+            return total;
+        }
+    }
+}
---
+++ Practice/Maths/Max69Number/CodeFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Maths.Max69Number
+{
+    class CodeFile
+    {
+        public int Maximum69Number(int num)
+        {
+            var charArray = num.ToString().ToCharArray();
+            for (int i = 0; i < charArray.Length; i++)
+                if (charArray[i] == '6')
+                {
+                    charArray[i] = '9';
+                    break;
+                }
+
+            return int.Parse(new string(charArray));
+        }
+    }
+}
