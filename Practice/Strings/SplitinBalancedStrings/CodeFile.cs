e386dbbea5b3eceba684696d23c2153cb75d33c5
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-05-27 22:18:58 -0400

    Adding code of string problems

---
+++ Practice/Strings/SplitinBalancedStrings/CodeFile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Strings.SplitinBalancedStrings
+{
+    class CodeFile
+    {
+        public int BalancedStringSplit(string s)
+        {
+            var result = 0;
+            var temp = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'L')
+                {
+                    temp++;
+                }
+                else
+                {
+                    temp--;
+                }
+
+                if (temp == 0)
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Practice/Strings/ValidPangramString/CodeFile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Strings
+{
+    class CodeFile
+    {
+        public bool CheckIfPangram(string sentence)
+        {
+            var cnt = new int[26];
+            var total = 0;
+            foreach (var ch in sentence)
+            {
+                if (++cnt[ch - 'a'] == 1)
+                {
+                    total++;
+                }
+            }
+            return total == 26;
+        }
+    }
+}
