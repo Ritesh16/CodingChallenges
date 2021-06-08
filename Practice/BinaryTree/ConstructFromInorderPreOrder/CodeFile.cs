687d557581b15f6726aa1fe8264aa783f57c310d
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-06-07 22:30:32 -0400

    Adding problems for Binary Tree

---
+++ Practice/BinaryTree/ConstructFromInorderPreOrder/CodeFile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practice.BinaryTree.ConstructFromInorderPreOrder
+{
+    public class CodeFile
+    {
+        public TreeNode BuildTree(int[] preorder, int[] inorder)
+        {
+            var queue = new Queue<int>(preorder);
+            var hashTable = inorder.Select((val, index) => (val, index))
+                                   .ToDictionary(x => x.val, x => x.index);
+
+            return Build(0, inorder.Length - 1);
+
+
+            TreeNode Build(int left, int right)
+            {
+                if (left > right)
+                    return null;
+
+                var last = queue.Dequeue();
+                var index = hashTable[last];
+
+                var root = new TreeNode(last);
+
+
+                root.left = Build(left, index - 1);
+                root.right = Build(index + 1, right);
+
+                return root;
+            }
+        }
+    }
+}
---
+++ Practice/BinaryTree/PopulatingRightPointers/CodeFileApproach2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.BinaryTree.PopulatingRightPointers
+{
+    class CodeFileApproach2
+    {
+        public Node Connect(Node root)
+        {
+            if (root == null)
+            {
+                return root;
+            }
+
+            Node leftMost = root;
+            while (leftMost.left != null)
+            {
+                var head = leftMost;
+                while (head != null)
+                {
+                    head.left.next = head.right;
+
+                    if (head.next != null)
+                    {
+                        head.right.next = head.next.left;
+                    }
+
+                    head = head.next;
+                }
+
+                leftMost = leftMost.left;
+            }
+
+            return root;
+        }
+    }
+}
