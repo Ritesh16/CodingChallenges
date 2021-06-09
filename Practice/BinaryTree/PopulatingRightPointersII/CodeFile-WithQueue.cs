98f1b695f76d3331d45f687961c5883b3bce2519
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-06-08 22:21:08 -0400

    Adding code for populating right pointer

---
+++ Practice/BinaryTree/PopulatingRightPointersII/CodeFile-WithQueue.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.BinaryTree.PopulatingRightPointersII
+{
+    class CodeFile_WithQueue
+    {
+        public Node Connect(Node root)
+        {
+            if (root == null)
+            {
+                return root;
+            }
+
+            var queue = new Queue<Node>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                var size = queue.Count;
+
+                for (int i = 0; i < size; i++)
+                {
+                    var node = queue.Dequeue();
+                    if (i < size - 1)
+                    {
+                        node.next = queue.Peek();
+                    }
+
+                    if (node.left != null) queue.Enqueue(node.left);
+                    if (node.right != null) queue.Enqueue(node.right);
+                }
+            }
+
+            return root;
+        }
+    }
+}
---
+++ Practice/BinaryTree/PopulatingRightPointersII/CodeFile-WithoutQueue.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.BinaryTree.PopulatingRightPointersII
+{
+    class CodeFile_WithoutQueue
+    {
+        public Node Connect(Node root)
+        {
+            if (root == null)
+            {
+                return root;
+            }
+
+            Node leftMost = null, current = null, previous = null;
+            leftMost = root;
+
+            while (leftMost != null)
+            {
+                previous = null;
+                current = leftMost;
+                leftMost = null;
+
+                while (current != null)
+                {
+                    ProcessNode(current.left);
+                    ProcessNode(current.right);
+                    current = current.next;
+                }
+            }
+
+            void ProcessNode(Node child)
+            {
+                if (child == null)
+                {
+                    return;
+                }
+
+                if (previous == null)
+                {
+                    leftMost = child;
+                }
+                else
+                {
+                    previous.next = child;
+                }
+
+                previous = child;
+            }
+
+
+            return root;
+        }
+    }
+}
