4b49a3ab284a336b99d831029d10575a017a849d
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-06-09 21:56:33 -0400

    Code for LCA

---
+++ Practice/BinaryTree/LCA/CodeFile-Iterative.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.BinaryTree.LCA
+{
+    public class CodeFile_Iterative
+    {
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            stack.Push(root);
+
+            var parent = new Dictionary<TreeNode, TreeNode>();
+            parent.Add(root, null);
+
+            while (!(parent.ContainsKey(p)) || !parent.ContainsKey(q))
+            {
+                var node = stack.Pop();
+
+                if (node.left != null)
+                {
+                    parent.Add(node.left, node);
+                    stack.Push(node.left);
+                }
+
+                if (node.right != null)
+                {
+                    parent.Add(node.right, node);
+                    stack.Push(node.right);
+                }
+            }
+
+            var ancestors = new HashSet<TreeNode>();
+            while (p != null)
+            {
+                ancestors.Add(p);
+                p = parent[p];
+            }
+
+            while (!ancestors.Contains(q))
+            {
+                q = parent[q];
+            }
+
+            return q;
+        }
+
+    }
+}
---
+++ Practice/BinaryTree/LCA/CodeFile-Recursive.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.BinaryTree.LCA
+{
+    public class CodeFile_Recursive
+    {
+        TreeNode ans;
+
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            RecurseTree(root, p, q);
+            return ans;
+        }
+
+        public bool RecurseTree(TreeNode root, TreeNode p, TreeNode q)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+
+            int left = RecurseTree(root.left, p, q) ? 1 : 0;
+
+            int right = RecurseTree(root.right, p, q) ? 1 : 0;
+
+            int mid = (root == p || root == q) ? 1 : 0;
+
+            if (left + right + mid >= 2)
+            {
+                ans = root;
+            }
+
+            return (left + right + mid) > 0;
+        }
+
+    }
+}
