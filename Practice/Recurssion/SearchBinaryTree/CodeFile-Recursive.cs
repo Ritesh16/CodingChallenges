5fd9df131b1edff798aab3d0199798beafbd7fa6
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-06-22 22:07:34 -0400

    Adding code of Searching Binary Tree

---
+++ Practice/Recurssion/SearchBinaryTree/CodeFile-Recursive.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Recurssion.SearchBinaryTree
+{
+    class CodeFile_Recursive
+    {
+        public TreeNode SearchBST(TreeNode root, int val)
+        {
+
+            if (root == null || root.val == val)
+            {
+                return root;
+            }
+
+            if (root.val < val)
+            {
+                return SearchBST(root.right, val);
+            }
+            else
+            {
+                return SearchBST(root.left, val);
+            }
+        }
+    }
+}
---
+++ Practice/Recurssion/SearchBinaryTree/CodeFile.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Recurssion.SearchBinaryTree
+{
+    class CodeFile
+    {
+        public TreeNode SearchBST(TreeNode root, int val)
+        {
+            while (root != null && val != root.val)
+            {
+                root = val < root.val ? root.left : root.right;
+            }
+
+            return root;
+        }
+        public TreeNode SearchBST(TreeNode root, int val)
+        {
+            if (root == null)
+            {
+                return root;
+            }
+
+            TreeNode result = null;
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+                if (node.val == val)
+                {
+                    result = node;
+                    break;
+                }
+
+                if (node.left != null)
+                    queue.Enqueue(node.left);
+
+                if (node.right != null)
+                    queue.Enqueue(node.right);
+            }
+
+            return result;
+        }
+    }
+}
