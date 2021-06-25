0abe369c690669883edc1a738218b42096b935ea
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-06-24 21:40:55 -0400

    Added code for Pascal Triangle ll

---
+++ Practice/Recurssion/PascalTrianglell/CodeFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Recurssion.PascalTrianglell
+{
+    public class CodeFile
+    {
+        public IList<int> GetRow(int rowIndex)
+        {
+            var output = new List<int>();
+
+            for (int i = 0; i <= rowIndex; i++)
+            {
+                output.Add(GetNum(rowIndex, i));
+            }
+
+            return output;
+        }
+
+        public int GetNum(int row, int col)
+        {
+            if (row == 0 || col == 0 || row == col)
+            {
+                return 1;
+            }
+
+            return GetNum(row - 1, col - 1) + GetNum(row - 1, col);
+        }
+    }
+}
