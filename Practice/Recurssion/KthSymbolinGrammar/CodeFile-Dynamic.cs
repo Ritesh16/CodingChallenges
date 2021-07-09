e9a81c399dc7d79f34af48c56e112debf8933e71
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-07-08 22:10:07 -0400

    Adding code for kth grammer

---
+++ Practice/Recurssion/KthSymbolinGrammar/CodeFile-Dynamic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Recurssion.KthSymbolinGrammar
+{
+    class CodeFile_Dynamic
+    {
+        public int KthGrammar(int N, int K)
+        {
+            if (N == 1)
+            {
+                return 0;
+            }
+
+            var parent = KthGrammar(N - 1, (K / 2) + (K % 2));
+            var isOdd = !(K % 2 == 0);
+
+            if (parent == 1)
+            {
+                return isOdd ? 1 : 0;
+            }
+            else
+            {
+                return isOdd ? 0 : 1;
+            }
+
+        }
+    }
+}
---
+++ Practice/Recurssion/KthSymbolinGrammar/CodeFile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Recurssion.KthSymbolinGrammar
+{
+    class CodeFile
+    {
+		public int KthGrammar(int N, int K)
+		{
+			N = N - 1;
+			K = K - 1;
+			return F(N, K);
+		}
+
+		public int F(int n, int k)
+		{
+			if (n == 0) return 0;
+			if (k % 2 == 0)
+			{
+				return F(n - 1, k / 2);
+			}
+			return F(n - 1, k / 2) == 1 ? 0 : 1;
+		}
+	}
+}
