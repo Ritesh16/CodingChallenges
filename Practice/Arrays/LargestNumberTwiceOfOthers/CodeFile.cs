bb5d8c39854e3909c4c43ca801012d191d38fa55
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-07-13 21:54:09 -0400

    Adding problem of Arrays

---
+++ Practice/Arrays/LargestNumberTwiceOfOthers/CodeFile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Arrays.LargestNumberTwiceOfOthers
+{
+    class CodeFile
+    {
+        public int DominantIndex(int[] nums)
+        {
+            var largest = 0;
+            var index = -1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (largest < nums[i])
+                {
+                    largest = nums[i];
+                    index = i;
+                }
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != largest && nums[i] * 2 > largest)
+                {
+                    index = -1;
+                    break;
+                }
+            }
+
+            return index;
+        }
+    }
+}
