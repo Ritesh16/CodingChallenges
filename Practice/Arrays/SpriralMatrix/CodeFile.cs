3cfad7496a466b7873c407a15602b38b71fef9ca
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-07-15 20:48:29 -0400

    Adding problems for SpiralMatrix and Tribonacci number

---
+++ Practice/Arrays/SpriralMatrix/CodeFile.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Arrays.SpriralMatrix
+{
+    class CodeFile
+    {
+        public IList<int> SpiralOrder(int[][] matrix)
+        {
+            var result = new List<int>();
+
+            var rowCount = matrix.Length - 1;
+            if (rowCount == -1) return result;
+
+            int colCount = matrix[0].Length - 1;
+            int rowStart = 0;
+            int colStart = 0; ;
+            while (rowStart <= rowCount && colStart <= colCount)
+            {
+                for (int i = colStart; i <= colCount; i++)
+                {
+                    result.Add(matrix[rowStart][i]);
+                }
+
+                for (int i = rowStart + 1; i <= rowCount; i++)
+                {
+                    result.Add(matrix[i][colCount]);
+                }
+
+                if (rowStart < rowCount && colStart < colCount)
+                {
+                    for (int i = colCount - 1; i > colStart; i--)
+                    {
+                        result.Add(matrix[rowCount][i]);
+                    }
+
+                    for (int i = rowCount; i > rowStart; i--)
+                    {
+                        result.Add(matrix[i][colStart]);
+                    }
+                }
+
+                rowStart++;
+                rowCount--;
+                colStart++;
+                colCount--;
+            }
+            return result;
+        }
+    }
+}
---
+++ Practice/Recurssion/TribonacciNumber/CodeFile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Recurssion.TribonacciNumber
+{
+    class CodeFile
+    {
+        public int Tribonacci(int n)
+        {
+            if (n < 2)
+            {
+                return n;
+            }
+
+            if (n == 2)
+            {
+                return 1;
+            }
+
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            map.Add(0, 0);
+            map.Add(1, 1);
+            map.Add(2, 1);
+
+            for (int i = 3; i <= n; i++)
+            {
+
+                var result = map[i - 1] + map[i - 2] + map[i - 3];
+                map.Add(i, result);
+
+            }
+
+            return map[n];
+
+
+        }
+    }
+}
