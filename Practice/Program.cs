137b524199764f2e368b5e322a39ecc4a998c760
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-07-16 21:25:37 -0400

    Adding problem for Longest Common PRefix-Vertical Scan

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using Practice.Recurssion.UniqueBinarySearchTrees;
+using Practice.Strings.LongestCommonPrefix;
 //using Practice.StacksAndQueues.NumberofIslands;
 
 namespace Practice
@@ -10,38 +10,14 @@ namespace Practice
     {
         static void Main(string[] args)
         {
-            //CodeFile_UnionFind ob = new CodeFile_UnionFind();
-            //char[][] input = new char[4][];
-            //input[0] = new char[3] { '1', '1', '1' };
-            //input[1] = new char[3] { '1', '1', '0' };
-            //input[2] = new char[3] { '1', '1', '0' };
-            //input[3] = new char[3] { '1', '0', '0' };
 
-            //var count = ob.NumIslands(input);
-            //Console.WriteLine(count);
+            CodeFile_VerticalScan ob = new CodeFile_VerticalScan();
+            var input = new string[] { "ab", "a" };
 
-            //ListNode node = new ListNode(1);
-            //node.next = new ListNode(2);
-            //node.next.next = new ListNode(3);
-
-            //CodeFile_Pointers ob = new CodeFile_Pointers();
-            //var n = ob.MiddleNode(node);
-            CodeFile ob = new CodeFile();
-            var result = ob.NumTrees(5);
-
-            CodeFile_Recurssion obr = new CodeFile_Recurssion();
-            var r1 = obr.NumTrees(5);
-            if (r1 == result)
-            {
-                Console.WriteLine("Match");
-            }
-            else
-            {
-                Console.WriteLine($"First:{result}, Second:{r1}");
-            }
+            var result = ob.LongestCommonPrefix(input);
+            Console.WriteLine(result);
 
             Console.ReadLine();
-            //Console.WriteLine(n);
         }
     }
 }
---
+++ Practice/Strings/LongestCommonPrefix/CodeFile-VerticalScan.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Strings.LongestCommonPrefix
+{
+    class CodeFile_VerticalScan
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            for (int i = 0; i < strs[0].Length; i++)
+            {
+                char val1 = strs[0][i];
+                for (int j = 1; j < strs.Length; j++)
+                {
+                    if (i == strs[j].Length || val1 != strs[j][i])
+                    {
+                        return strs[0].Substring(0, i);
+                    }
+                }
+            }
+
+            return strs[0];
+        }
+    }
+}
