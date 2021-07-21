04964118542ec6b80a841088e5fdd427acff95ed
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-07-21 17:14:17 -0400

    Adding new solutions for LongestCommonPrefix

---
+++ Practice/Strings/LongestCommonPrefix/CodeFile-BinarySearch.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Strings.LongestCommonPrefix
+{
+    class CodeFile_BinarySearch
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            if (strs == null || strs.Length <= 0)
+            {
+                return "";
+            }
+
+            var minLength = int.MaxValue;
+            foreach (var item in strs)
+            {
+                minLength = Math.Min(item.Length, minLength);
+            }
+
+
+
+            var start = 1;
+            var end = minLength;
+
+            while (start <= end)
+            {
+                var mid = (start + end) / 2;
+                if (CommonPrefixFound(strs, mid))
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid - 1;
+                }
+            }
+
+            return strs[0].Substring(0, (start + end) / 2);
+        }
+
+        private bool CommonPrefixFound(string[] strs, int mid)
+        {
+            var str1 = strs[0].Substring(0, mid);
+            for (int i = 1; i < strs.Length; i++)
+                if (!strs[i].StartsWith(str1))
+                    return false;
+            return true;
+        }
+    }
+}
---
+++ Practice/Strings/LongestCommonPrefix/CodeFile-DivideConquer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Strings.LongestCommonPrefix
+{
+    public class CodeFile_DivideConquer
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            if (strs == null || strs.Length <= 0)
+            {
+                return "";
+            }
+
+            return FindLCP(strs, 0, strs.Length - 1);
+        }
+
+        private string FindLCP(string[] strs, int start, int end)
+        {
+            if (start == end)
+            {
+                return strs[start];
+            }
+            else
+            {
+                var mid = (start + end) / 2;
+                var firstPrefix = FindLCP(strs, start, mid);
+                var secondPrefix = FindLCP(strs, mid + 1, end);
+                return LongestCommonPrefix(firstPrefix, secondPrefix);
+            }
+
+        }
+
+        private string LongestCommonPrefix(string firstPrefix, string secondPrefix)
+        {
+            int minLength = Math.Min(firstPrefix.Length, secondPrefix.Length);
+            for (int i = 0; i < minLength; i++)
+            {
+                if (firstPrefix[i] != secondPrefix[i])
+                {
+                    return firstPrefix.Substring(0, i);
+                }
+            }
+
+            return firstPrefix.Substring(0, minLength);
+        }
+    }
+}
