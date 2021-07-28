1b5b8b36242debef54fe1061eaeeb307d5b000a4
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-07-27 21:52:39 -0400

    Adding code for binary search

---
+++ Practice/BinarySearch/Sqrt(x)/CodeFile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.BinarySearch.Sqrt_x_
+{
+    class CodeFile
+    {
+        public int MySqrt_Inbuilt(int x)
+        {
+            return (int)Math.Floor(Math.Sqrt(x));
+        }
+
+        public int MySqrt(int x)
+        {
+            if (x < 2)
+                return x;
+
+            int left = 1, right = x / 2;
+
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+
+                if (mid == x / mid)
+                    return mid;
+                else if (mid < x / mid)
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+
+            return right;
+        }
+    }
+}
---
+++ Practice/BinarySearch/Sqrt(x)/Readme.txt
@@ -0,0 +1,56 @@
+﻿Given a non-negative integer x, compute and return the square root of x.
+
+Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+
+Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5.
+
+ 
+
+Example 1:
+
+Input: x = 4
+Output: 2
+Example 2:
+
+Input: x = 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since the decimal part is truncated, 2 is returned.
+ 
+
+Constraints:
+
+0 <= x <= 231 - 1
+
+0 ~ 6 months6 months ~ 1 year1 year ~ 2 years
+
+LinkedIn
+|
+5
+
+Bloomberg
+|
+4
+
+Microsoft
+|
+4
+
+Google
+|
+3
+
+Amazon
+|
+3
+
+Apple
+|
+2
+
+Adobe
+|
+2
+
+ByteDance
+|
+2
