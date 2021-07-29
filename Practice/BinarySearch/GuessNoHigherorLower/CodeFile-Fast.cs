320c32892df92a454391c35fc757c89e4c05757d
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-07-28 21:51:13 -0400

    Adding problem Guess number higher or lower

---
+++ Practice/BinarySearch/GuessNoHigherorLower/CodeFile-Fast.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.BinarySearch.GuessNoHigherorLower
+{
+    class CodeFile_Fast : GuessGame
+    {
+        public int GuessNumber(int n)
+        {
+            int i = 1, j = n;
+            while (i <= j)
+            {
+                int mid = i + (j - i) / 2;
+                if (guess(mid) > 0)
+                    i = mid + 1;
+                else if (guess(mid) < 0)
+                    j = mid - 1;
+                else
+                    return mid;
+            }
+            return -1;
+        }
+    }
+}
---
+++ Practice/BinarySearch/GuessNoHigherorLower/CodeFile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.BinarySearch.GuessNoHigherorLower
+{
+    class CodeFile : GuessGame
+    {
+        public int GuessNumber(int n)
+        {
+            int i = 1, j = n;
+            while (i <= j)
+            {
+                int mid = i + (j - i) / 2;
+                if (guess(mid) > 0)
+                    i = mid + 1;
+                else if (guess(mid) < 0)
+                    j = mid - 1;
+                else
+                    return mid;
+            }
+            return -1;
+        }
+    }
+
+    internal class GuessGame
+    {
+        internal int guess(int mid)
+        {
+            return 0;
+        }
+    }
+}
