7cc2a3a9b4e155ea29e9206da4296ced5207a9c2
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-08-03 22:13:29 -0400

    Adding code of Find First and Last Position of Element in Sorted Array

---
+++ Practice/BinarySearch/FindFirstLastPositionElementinSortedArray/CodeFile.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.BinarySearch.FindFirstLastPositionElementinSortedArray
+{
+    class CodeFile
+    {
+        public int[] SearchRange(int[] nums, int target)
+        {
+            var first = Search(nums, target, true);
+            if (first == -1)
+            {
+                return new int[] { -1, -1 };
+            }
+
+            var second = Search(nums, target, false);
+            return new int[] { first, second };
+        }
+
+        private int Search(int[] nums, int target, bool firstOccurrence)
+        {
+            var begin = 0;
+            var end = nums.Length - 1;
+
+            while (begin <= end)
+            {
+                var mid = begin + (end - begin) / 2;
+
+                if (nums[mid] == target)
+                {
+                    if (firstOccurrence)
+                    {
+                        if (mid == begin || nums[mid - 1] != target)
+                        {
+                            return mid;
+                        }
+
+                        end = mid - 1;
+                    }
+                    else
+                    {
+                        if (mid == end || nums[mid + 1] != target)
+                        {
+                            return mid;
+                        }
+
+                        begin = mid + 1;
+                    }
+                }
+                else if (nums[mid] > target)
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    begin = mid + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Practice/BinarySearch/FindFirstLastPositionElementinSortedArray/Readme.txt
@@ -0,0 +1,70 @@
+﻿Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+
+If target is not found in the array, return [-1, -1].
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+Example 2:
+
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+Example 3:
+
+Input: nums = [], target = 0
+Output: [-1,-1]
+ 
+
+Constraints:
+
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+nums is a non-decreasing array.
+-109 <= target <= 109
+
+0 ~ 6 months6 months ~ 1 year1 year ~ 2 years
+
+Facebook
+|
+30
+
+LinkedIn
+|
+9
+
+Uber
+|
+5
+
+Microsoft
+|
+4
+
+Apple
+|
+3
+
+Google
+|
+3
+
+Bloomberg
+|
+2
+
+Amazon
+|
+2
+
+Twitter
+|
+2
+
+Intuit
+|
+2
