a7f8e79a06a4af10b62c7119bb5dc699a18f0b08
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-08-06 22:07:57 -0400

    Adding BinarySearch 1. Find K Closest Elements

---
+++ Practice/BinarySearch/FindKClosestElements/CodeFile-Binary.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.BinarySearch.FindKClosestElements
+{
+    class CodeFile_Binary
+    {
+        public IList<int> FindClosestElements(int[] arr, int k, int x)
+        {
+            var result = new List<int>();
+            if (arr.Length == k)
+            {
+                result.AddRange(arr);
+                return result;
+            }
+
+            var left = 0;
+            var right = arr.Length;
+            while (left < right)
+            {
+                var mid = (left + right) / 2;
+                if (arr[mid] >= x)
+                {
+                    right = mid;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+
+
+            left -= 1;
+            right = left + 1;
+            while (right - left - 1 < k)
+            {
+                if (left == -1)
+                {
+                    right += 1;
+                    continue;
+                }
+
+                if (right == arr.Length || Math.Abs(arr[left] - x) <= Math.Abs(arr[right] - x))
+                {
+                    left -= 1;
+                }
+                else
+                {
+                    right += 1;
+                }
+            }
+
+            for (int i = left + 1; i < right; i++)
+            {
+                result.Add(arr[i]);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Practice/BinarySearch/FindKClosestElements/CodeFile-BinaryIntutive.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.BinarySearch.FindKClosestElements
+{
+    class CodeFile_BinaryIntutive
+    {
+        public IList<int> FindClosestElements(int[] arr, int k, int x)
+        {
+            var left = 0;
+            var right = arr.Length - k;
+
+            while (left < right)
+            {
+                var mid = (left + right) / 2;
+                if (x - arr[mid] > arr[mid + k] - x)
+                {
+                    left += 1;
+                }
+                else
+                {
+                    right = mid;
+                }
+            }
+
+            var result = new List<int>();
+            for (int i = left; i < left + k; i++)
+            {
+                result.Add(arr[i]);
+            }
+
+            return result;
+        }
+    }
+}
