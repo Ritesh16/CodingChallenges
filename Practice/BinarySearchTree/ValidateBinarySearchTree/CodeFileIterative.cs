112fb136f41b7b5e56ec3819ef838cfd28a1220e
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-08-16 21:59:45 -0400

    Adding problem for BST 1. Validate BST

---
+++ Practice/BinarySearchTree/ValidateBinarySearchTree/CodeFileIterative.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.BinarySearchTree.ValidateBinarySearchTree
+{
+    class CodeFileIterative
+    {
+        public bool IsValidBST(TreeNode root)
+        {
+            var stack = new Stack<TreeNode>();
+            var lower = new Stack<int?>();
+            var upper = new Stack<int?>();
+
+            stack.Push(root);
+            lower.Push(null);
+            upper.Push(null);
+
+            while (stack.Count != 0)
+            {
+                var node = stack.Pop();
+                var low = lower.Pop();
+                var high = upper.Pop();
+                if (node == null) continue;
+                if ((low.HasValue && node.val <= low) || (high.HasValue && node.val >= high))
+                {
+                    return false;
+                }
+
+
+                stack.Push(node.right);
+                lower.Push(node.val);
+                upper.Push(high);
+                stack.Push(node.left);
+                upper.Push(node.val);
+                lower.Push(low);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Practice/BinarySearchTree/ValidateBinarySearchTree/CodeFileRecursive.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.BinarySearchTree.ValidateBinarySearchTree
+{
+    class CodeFileRecursive
+    {
+        public bool IsValidBST(TreeNode root)
+        {
+            return Validate(root, null, null);
+        }
+
+        public bool Validate(TreeNode root, int? low, int? high)
+        {
+            if (root == null)
+            {
+                return true;
+            }
+
+            if ((low.HasValue && root.val <= low) || (high.HasValue && root.val >= high))
+            {
+                return false;
+            }
+
+            return Validate(root.right, root.val, high) && Validate(root.left, low, root.val);
+        }
+    }
+}
