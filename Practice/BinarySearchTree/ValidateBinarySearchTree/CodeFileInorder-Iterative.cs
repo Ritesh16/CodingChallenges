48ce0910ceb16eeb8d8c76cdb621031b3918dc02
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-08-17 21:38:47 -0400

    Adding BST 1. Validate BST, using inorder

---
+++ Practice/BinarySearchTree/ValidateBinarySearchTree/CodeFileInorder-Iterative.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.BinarySearchTree.ValidateBinarySearchTree
+{
+    class CodeFileInorder_Iterative
+    {
+        int? lastValue = null;
+        public bool IsValidBST(TreeNode root)
+        {
+            return ValidateInOrder_Iterative(root);
+        }
+
+        public bool ValidateInOrder_Iterative(TreeNode root)
+        {
+            if (root == null)
+            {
+                return true;
+            }
+
+            var result = true;
+            var stack = new Stack<TreeNode>();
+            var node = root;
+
+            while (node != null || stack.Count != 0)
+            {
+                while (node != null)
+                {
+                    stack.Push(node);
+                    node = node.left;
+                }
+
+                node = stack.Pop();
+
+                if (lastValue.HasValue && node.val <= lastValue)
+                {
+                    result = false;
+                    break;
+                }
+
+                lastValue = node.val;
+                node = node.right;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Practice/BinarySearchTree/ValidateBinarySearchTree/CodeFileInorder-Recursive.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.BinarySearchTree.ValidateBinarySearchTree
+{
+    class CodeFileInorder_Recursive
+    {
+        int? lastValue = null;
+        public bool IsValidBST(TreeNode root)
+        {
+            return ValidateInOrder(root);
+        }
+
+        public bool ValidateInOrder(TreeNode root)
+        {
+            if (root == null)
+            {
+                return true;
+            }
+
+            if (!ValidateInOrder(root.left))
+            {
+                return false;
+            }
+
+            if (lastValue.HasValue && root.val <= lastValue)
+            {
+                return false;
+            }
+
+            lastValue = root.val;
+            return ValidateInOrder(root.right);
+        }
+    }
+}
