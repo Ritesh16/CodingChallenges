98c0ebd7443507252baa4c1fd1e1f07c39789b17
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-08-27 17:46:12 -0400

    Adding code for BST 1. Insert in BST

---
+++ Practice/BinarySearchTree/InsertInBST/CodeFile-Iterative.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.BinarySearchTree.InsertInBST
+{
+    class CodeFile_Iterative
+    {
+        public TreeNode InsertIntoBST(TreeNode root, int val)
+        {
+            return Insert(root, val);
+        }
+        public TreeNode Insert(TreeNode root, int val)
+        {
+            TreeNode newNode = new TreeNode(val);
+            if (root == null)
+            {
+                return newNode;
+            }
+
+            TreeNode curr = root;
+            TreeNode target = curr;
+
+            while (curr != null)
+            {
+                target = curr;
+                curr = (curr.val >= val) ? curr.left : curr.right;
+            }
+
+            if (target.val >= val)
+                target.left = newNode;
+            else
+                target.right = newNode;
+
+            return root;
+        }
+    }
+}
---
+++ Practice/BinarySearchTree/InsertInBST/CodeFile-Recursive.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.BinarySearchTree.InsertInBST
+{
+    class CodeFile_Recursive
+    {
+        public TreeNode InsertIntoBST(TreeNode root, int val)
+        {
+            return Insert(root, val);
+        }
+        public TreeNode Insert(TreeNode root, int val)
+        {
+            if (root == null) return new TreeNode(val);
+
+            if (val <= root.val)
+                root.left = InsertIntoBST(root.left, val);
+            else
+                root.right = InsertIntoBST(root.right, val);
+
+            return root;
+        }
+    }
+}
