47973d5c168d7ba82ffd6d6906b264b16f493b69
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-09-19 17:27:03 -0400

    Adding code for deleting node in BST

---
+++ Practice/BinarySearchTree/DeleteBST/CodeFile-Fast.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.BinarySearchTree.DeleteBST
+{
+    class CodeFile_Fast
+    {
+        public TreeNode DeleteNode(TreeNode root, int key)
+        {
+            if (root == null) return null;
+
+            if (key > root.val)
+            {
+                root.right = DeleteNode(root.right, key);
+            }
+            else if (key < root.val)
+            {
+                root.left = DeleteNode(root.left, key);
+            }
+            else
+            {
+                if (root.left == null && root.right == null) root = null;
+                else if (root.right != null)
+                {
+                    root.val = FindSuccessor(root);
+                    root.right = DeleteNode(root.right, root.val);
+                }
+                else
+                {
+                    root.val = FindPredessor(root);
+                    root.left = DeleteNode(root.left, root.val);
+                }
+            }
+
+            return root;
+        }
+
+        private int FindPredessor(TreeNode root)
+        {
+            root = root.left;
+            while (root.right != null) root = root.right;
+            return root.val;
+        }
+
+        private int FindSuccessor(TreeNode root)
+        {
+            root = root.right;
+            while (root.left != null) root = root.left;
+            return root.val;
+        }
+    }
+}
---
+++ Practice/BinarySearchTree/DeleteBST/CodeFile.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.BinarySearchTree.DeleteBST
+{
+    class CodeFile
+    {
+        public TreeNode DeleteNode(TreeNode root, int key)
+        {
+            TreeNode parent = null;
+            TreeNode node = root;
+
+            while (node != null && node.val != key)
+            {
+                parent = node;
+                node = node.val > key ? node.left : node.right;
+            }
+
+            if (node == null) return root;
+
+            TreeNode replacement = FindReplacement(node.left, node.right);
+
+            if (parent == null)
+            {
+                return replacement;
+            }
+            else
+            {
+                if (parent.left == node) parent.left = replacement;
+                if (parent.right == node) parent.right = replacement;
+            }
+
+            return root;
+        }
+
+        private TreeNode FindReplacement(TreeNode left, TreeNode right)
+        {
+            if (left == null && right == null) return null;
+            if (left == null) return right;
+            if (right == null) return left;
+
+            TreeNode head = left;
+            while (left.right != null) left = left.right;
+
+            left.right = right;
+            return head;
+        }
+    }
+}
