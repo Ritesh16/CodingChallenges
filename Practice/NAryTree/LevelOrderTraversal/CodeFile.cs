2ce835ed4fbcf737c9f60b813e3dbbaf0bee9026
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-09-26 15:58:12 -0400

    Adding code for NAryTree-LevelOrder

---
+++ Practice/NAryTree/LevelOrderTraversal/CodeFile.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.NAryTree.LevelOrderTraversal
+{
+    public class CodeFile
+    {
+        public IList<IList<int>> LevelOrder(Node root)
+        {
+            var output = new List<IList<int>>();
+            var queue = new Queue<Node>();
+            if (root == null)
+            {
+                return output;
+            }
+
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                var size = queue.Count;
+                var list = new List<int>();
+                for (int i = 0; i < size; i++)
+                {
+                    var node = queue.Dequeue();
+
+
+                    list.Add(node.val);
+                    if (node.children != null)
+                    {
+                        foreach (var item in node.children)
+                        {
+                            queue.Enqueue(item);
+                        }
+
+                    }
+                }
+
+                output.Add(list);
+            }
+
+            return output;
+        }
+    }
+}
