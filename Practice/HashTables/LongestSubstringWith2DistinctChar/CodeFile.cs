1092def91d5f937ad343f5ae6fa56b11cb2eb511
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-10-07 21:05:36 -0400

    Adding problem of Longest Substring with atmost 2 distinct charcaters

---
+++ Practice/HashTables/LongestSubstringWith2DistinctChar/CodeFile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.HashTables.LongestSubstringWith2DistinctChar
+{
+    public class CodeFile
+    {
+        public int LengthOfLongestSubstringTwoDistinct(string s)
+        {
+            if (s == null || s.Length == 0)
+                return 0;
+
+            var length = s.Length;
+            
+            // Sliding window
+            var left = 0;
+            var right = 0;
+            var map = new Dictionary<char, int>();
+            var maxLength = 0;
+
+            while (right < length)
+            {
+                map.TryAdd(s[right], 0);
+                map[s[right]]++;
+
+                while (left < right && map.Keys.Count > 2)
+                {
+                    map[s[left]]--;
+                    if (map[s[left]] == 0)
+                        map.Remove(s[left]);
+                    left++;
+                }
+
+                maxLength = Math.Max(maxLength, right - left+1);
+                right++;
+            }
+
+            return maxLength;
+        }
+    }
+}
--- Practice/Program.cs
+++ Practice/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using Practice.HashTables.LoggerRateLimiter;
+using Practice.HashTables.LongestSubstringWith2DistinctChar;
 
 namespace Practice
 {
@@ -10,17 +10,21 @@ namespace Practice
     {
         static void Main(string[] args)
         {
-            var logger = new Logger();
-            Console.WriteLine(logger.ShouldPrintMessage(0, "A"));  // return true, next allowed timestamp for "foo" is 1 + 10 = 11
-            Console.WriteLine(logger.ShouldPrintMessage(0, "B"));  // return true, next allowed timestamp for "bar" is 2 + 10 = 12
-            Console.WriteLine(logger.ShouldPrintMessage(0, "C"));
-            Console.WriteLine(logger.ShouldPrintMessage(0, "A"));// 3 < 11, return false
-            Console.WriteLine(logger.ShouldPrintMessage(0, "B"));  // return true, next allowed timestamp for "bar" is 2 + 10 = 12
-            Console.WriteLine(logger.ShouldPrintMessage(0, "C")); 
-            Console.WriteLine(logger.ShouldPrintMessage(11, "A"));
-            Console.WriteLine(logger.ShouldPrintMessage(11, "B"));
-            Console.WriteLine(logger.ShouldPrintMessage(11, "C"));
-            Console.WriteLine(logger.ShouldPrintMessage(11, "A"));
+            //var logger = new Logger();
+            //Console.WriteLine(logger.ShouldPrintMessage(0, "A"));  // return true, next allowed timestamp for "foo" is 1 + 10 = 11
+            //Console.WriteLine(logger.ShouldPrintMessage(0, "B"));  // return true, next allowed timestamp for "bar" is 2 + 10 = 12
+            //Console.WriteLine(logger.ShouldPrintMessage(0, "C"));
+            //Console.WriteLine(logger.ShouldPrintMessage(0, "A"));// 3 < 11, return false
+            //Console.WriteLine(logger.ShouldPrintMessage(0, "B"));  // return true, next allowed timestamp for "bar" is 2 + 10 = 12
+            //Console.WriteLine(logger.ShouldPrintMessage(0, "C")); 
+            //Console.WriteLine(logger.ShouldPrintMessage(11, "A"));
+            //Console.WriteLine(logger.ShouldPrintMessage(11, "B"));
+            //Console.WriteLine(logger.ShouldPrintMessage(11, "C"));
+            //Console.WriteLine(logger.ShouldPrintMessage(11, "A"));
+
+            var ob = new CodeFile();
+            var output = ob.LengthOfLongestSubstringTwoDistinct("eceba");
+            Console.WriteLine(output);
             Console.ReadLine();
         }
     }
