26cbf8002a38d5b5ec3ebbc2e45ef564a095e714
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-11-30 10:12:38 -0500

    Adding problem for  Final Value of Variable After Performing Operations Submission Detail

---
+++ Practice/Arrays/FinalValueAfterPerformingOperations/CodeFile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Arrays.FinalValueAfterPerformingOperations
+{
+    public class CodeFile
+    {
+        public int FinalValueAfterOperations(string[] operations)
+        {
+            var sum = 0;
+            for (int i = 0; i < operations.Length; i++)
+            {
+                if (operations[i] == "++X" || operations[i] == "X++")
+                {
+                    sum++;
+                }
+                else
+                {
+                    sum--;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
