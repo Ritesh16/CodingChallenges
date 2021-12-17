105b906f54cb06d0a6d9b3ad27d5ec7403e48bd0
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-12-17 10:44:40 -0500

    Adding problem of Minimum Number of Moves to Seat Everyone

---
+++ Practice/Arrays/MinNumberofMovestoSeatEveryone/CodeFile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Arrays.MinNumberofMovestoSeatEveryone
+{
+    public class CodeFile
+    {
+        public int MinMovesToSeat(int[] seats, int[] students)
+        {
+            var result = 0;
+            Array.Sort(seats);
+            Array.Sort(students);
+
+            for (int i = 0; i < seats.Length; i++)
+            {
+                result += Math.Abs(seats[i] - students[i]);
+            }
+
+            return result;
+        }
+    }
+}
