6093b874231682406c5e0d9e195382a62f32ff9f
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-12-21 14:35:12 -0500

    Adding problem for rings and rods-Hashtable

---
+++ Practice/HashTables/RingsAndRods/CodeFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.HashTables.RingsAndRods
+{
+    internal class CodeFile
+    {
+        public int CountPoints(string rings)
+        {
+            HashSet<char>[] hashSetArray = new HashSet<char>[10];
+            for (int i = 0; i < 10; i++)
+            {
+                hashSetArray[i] = new HashSet<char>();
+            }
+
+            var result = 0;
+
+            for (int i = 0; i < rings.Length; i = i + 2)
+            {
+                var index = rings[i + 1] - '0';
+
+                hashSetArray[index].Add(rings[i]);
+            }
+
+            result = hashSetArray.Count(x => x.Count == 3);
+
+            return result;
+        }
+    }
+}
