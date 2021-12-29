d149c3633665226f38ea9bb1f58cc70ea5e43b43
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-12-29 16:04:20 -0500

    Adding problem for Divisor game

---
+++ Practice/DynamicProgramming/Divisor Game/CodeFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.DynamicProgramming.Divisor_Game
+{
+    internal class CodeFile
+    {
+        public bool DivisorGame(int n)
+        {
+            if (n % 2 == 0)
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ Practice/DynamicProgramming/Divisor Game/CodeFileMyVersion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.DynamicProgramming.Divisor_Game
+{
+    internal class CodeFileMyVersion
+    {
+        // Dooes not work
+        public bool DivisorGame(int n)
+        {
+            var count = 0;
+            while (n > 1)
+            {
+                if (n % 2 == 0 && n != 2)
+                {
+                    n = n - 2;
+                }
+                else
+                {
+                    n = n - 1;
+                }
+
+                count++;
+            }
+
+            return !(count % 2 == 0);
+        }
+    }
+}
