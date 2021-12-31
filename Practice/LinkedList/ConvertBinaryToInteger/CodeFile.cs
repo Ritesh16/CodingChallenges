851a05240a64dbce43e4c3d81e4cbe0ee2774db8
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-12-31 12:25:50 -0500

    Adding problem of LinkedList

---
+++ Practice/LinkedList/ConvertBinaryToInteger/CodeFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.LinkedList.ConvertBinaryToInteger
+{
+    internal class CodeFile
+    {
+        public int GetDecimalValue(ListNode head)
+        {
+            var node = head;
+            var builder = new StringBuilder();
+            while (node != null)
+            {
+                builder.Append(node.val);
+                node = node.next;
+            }
+
+            return Convert.ToInt32(builder.ToString(), 2);
+        }
+    }
+}
