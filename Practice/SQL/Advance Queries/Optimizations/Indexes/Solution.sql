17d70607cc7b0f8daccf4980e341016b9d4a6ccd
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-01-09 10:51:07 -0500

    SQL : Using Indexes

---
+++ Practice/SQL/Advance Queries/Optimizations/Indexes/Solution.sql
@@ -0,0 +1,58 @@
+CREATE TABLE #Persons (
+	BusinessEntityID INT,
+	Title NVARCHAR(8),
+	FirstName NVARCHAR(50),
+	MiddleName NVARCHAR(50),
+	LastName NVARCHAR(50),
+	PhoneNumber NVARCHAR(20),
+	PhoneNumberType NVARCHAR(10),
+	EmailAddress NVARCHAR(100),
+	PhoneNumberTypeId INT
+)
+
+
+INSERT INTO #Persons (
+ BusinessEntityID,
+ Title,
+ FirstName,
+ MiddleName,
+ LastName
+)
+SELECT 
+	BusinessEntityID,
+	Title,
+	FirstName,
+	MiddleName,
+	LastName
+FROM Person.Person
+
+CREATE CLUSTERED INDEX Persons_idx ON #Persons(BusinessEntityID)
+
+UPDATE A
+SET PhoneNumber = B.PhoneNumber
+   , PhoneNumberTypeId = b.PhoneNumberTypeID
+FROM #Persons A
+JOIN Person.PersonPhone B
+	ON A.BusinessEntityID = B.BusinessEntityID
+
+
+
+CREATE nonCLUSTERED INDEX Persons_idx2 ON #Persons(PhoneNumberTypeId)
+
+UPDATE A
+SET PhoneNumberType = B.Name
+FROM #Persons A
+JOIN Person.PhoneNumberType B
+	ON A.PhoneNumberTypeId = B.PhoneNumberTypeID
+
+
+UPDATE A
+SET EmailAddress = B.EmailAddress
+FROM #Persons A
+JOIN Person.EmailAddress B
+	ON A.BusinessEntityID = B.BusinessEntityID
+
+
+SELECT * FROM #Persons
+
+DROP TABLE #Persons
