10b9d8c0f556051be41e09bb803244b5b2f85d86
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-01-11 15:06:29 -0500

    LinkedList: Swapping Linked List elements

---
+++ Practice/LinkedList/SwappingLinkedList/CodeFile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.LinkedList.SwappingLinkedList
+{
+    internal class CodeFile
+    {
+        public ListNode SwapNodes(ListNode head, int k)
+        {
+            var current = head;
+            ListNode start = null, trail = null;
+            var i = 1;
+
+            while (current != null)
+            {
+                if (trail != null)
+                {
+                    trail = trail.next;
+                }
+
+                if (i == k)
+                {
+                    start = current;
+                    trail = head;
+                }
+
+                current = current.next;
+                i++;
+            }
+
+            var temp = start.val;
+            start.val = trail.val;
+            trail.val = temp;
+
+            return head;
+        }
+    }
+}
