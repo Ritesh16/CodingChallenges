bb6ede55517a6ed2a521d7f15f327144d7730112
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-01-13 18:16:48 -0500

    LinkedList: Find next greater node

---
+++ Practice/LinkedList/NextGreaterNode/CodeFile-BruteForce.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.LinkedList.NextGreaterNode
+{
+    internal class CodeFile_BruteForce
+    {
+        public int[] NextLargerNodes(ListNode head)
+        {
+            var node = head;
+            var result = new List<int>();
+            var max = 0;
+
+            while (node != null)
+            {
+                max = 0;
+                var current = node.next;
+                while (current != null)
+                {
+                    if (node.val < current.val)
+                    {
+                        max = current.val;
+                        break;
+                    }
+
+
+                    current = current.next;
+                }
+
+
+                result.Add(max);
+                node = node.next;
+            }
+
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ Practice/LinkedList/NextGreaterNode/CodeFile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.LinkedList.NextGreaterNode
+{
+    internal class CodeFile
+    {
+        public int[] NextLargerNodes(ListNode head)
+        {
+            var current = head;
+            var list = new List<int>();
+
+            while (current != null)
+            {
+                list.Add(current.val);
+                current = current.next;
+            }
+
+            var result = new int[list.Count];
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                while (stack.Count != 0 && list[stack.Peek()] < list[i])
+                {
+                    result[stack.Pop()] = list[i];
+                }
+
+                stack.Push(i);
+            }
+
+            return result;
+        }
+    }
+}
