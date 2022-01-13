d26b341b9123f67e46b6b4c6b26fbb916ace6b78
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-01-12 21:20:53 -0500

    LinkedList: MaxTwinSum

---
+++ Practice/LinkedList/MaxTwinSum/CodeFile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.LinkedList.MaxTwinSum
+{
+    internal class CodeFile
+    {
+        public int PairSum(ListNode head)
+        {
+            var current = head;
+            var fast = head;
+            var stack = new Stack<int>();
+
+            while (fast != null && fast.next != null)
+            {
+                fast = fast.next.next;
+                stack.Push(current.val);
+                current = current.next;
+            }
+
+            var max = -1;
+            while (stack.Count > 0 && current != null)
+            {
+                var val = stack.Pop();
+                max = Math.Max(max, val + current.val);
+                current = current.next;
+            }
+
+            return max;
+        }
+    }
+}
