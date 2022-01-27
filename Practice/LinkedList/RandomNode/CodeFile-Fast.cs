4cd8e5d022d4e4b7f68556bb124dfd7b774c6144
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-01-27 11:09:36 -0500

    Linked List: Get Random node

---
+++ Practice/LinkedList/RandomNode/CodeFile-Fast.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.LinkedList.RandomNode
+{
+    internal class Solution_Fast
+    {
+        private ListNode head;
+        Random random = new Random();
+        public Solution_Fast(ListNode head)
+        {
+            this.head = head;
+        }
+
+        public int GetRandom()
+        {
+            var candidate = this.head.val;
+
+            var n = 1;
+
+            var cur = head.next;
+            n++;
+
+            while (cur != null)
+            {
+                var randomIndex = random.Next(n);
+                if (randomIndex == 0) candidate = cur.val;
+
+                cur = cur.next;
+                n++;
+            }
+            return candidate;
+        }
+    }
+}
---
+++ Practice/LinkedList/RandomNode/CodeFile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.LinkedList.RandomNode
+{
+    internal class Solution
+    {
+        List<int> list = new List<int>();
+        Random random = new Random();
+        public Solution(ListNode head)
+        {
+            while (head != null)
+            {
+                list.Add(head.val);
+                head = head.next;
+            }
+        }
+
+        public int GetRandom()
+        {
+            int num = random.Next(list.Count);
+            return list[num];
+        }
+    }
+}
