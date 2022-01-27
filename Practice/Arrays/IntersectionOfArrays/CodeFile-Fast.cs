09f4845a0f783536b0275cd5fa292c7c97d9366c
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-01-27 11:13:46 -0500

    Array: Intersection

---
+++ Practice/Arrays/IntersectionOfArrays/CodeFile-Fast.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Arrays.IntersectionOfArrays
+{
+    internal class CodeFile_Fast
+    {
+        public int[] Intersection(int[] nums1, int[] nums2)
+        {
+            HashSet<int> set1 = new HashSet<int>(nums1);
+            HashSet<int> set2 = new HashSet<int>();
+
+            foreach (int num in nums2)
+            {
+                if (set1.Contains(num))
+                {
+                    set2.Add(num);
+                }
+            }
+
+            return set2.ToArray();
+        }
+    }
+}
---
+++ Practice/Arrays/IntersectionOfArrays/CodeFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practice.Arrays.IntersectionOfArrays
+{
+    internal class CodeFile
+    {
+        public int[] Intersection(int[] nums1, int[] nums2)
+        {
+            return nums1.ToList().Intersect(nums2.ToList()).ToArray();
+        }
+    }
+}
