1f56523c0ae88398c81b8e7cb70a47e5bc01b70c
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-01-30 15:23:05 -0500

    Hashtable: Count number of pairs

---
+++ Practice/HashTables/CountNumberofPairs/CodeFile-Fast.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.HashTables.CountNumberofPairs
+{
+    internal class CodeFile_Fast
+    {
+        public int CountKDifference(int[] nums, int k)
+        {
+            int m = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (Math.Abs(nums[i] - nums[j]) == k)
+                        m++;
+                }
+            }
+            return m;
+        }
+    }
+}
---
+++ Practice/HashTables/CountNumberofPairs/CodeFile.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.HashTables.CountNumberofPairs
+{
+    internal class CodeFile
+    {
+        public int CountKDifference(int[] nums, int k)
+        {
+
+            if (nums == null || nums.Length < 2)
+            {
+                return 0;
+            }
+
+            var result = 0;
+            var map = new Dictionary<int, int>();
+            foreach (var item in nums)
+            {
+                if (map.ContainsKey(item - k))
+                {
+                    result += map[item - k];
+                }
+
+                if (map.ContainsKey(item + k))
+                {
+                    result += map[item + k];
+                }
+
+                if (map.ContainsKey(item))
+                {
+                    map[item]++;
+                }
+                else
+                {
+                    map.Add(item, 1);
+                }
+
+            }
+
+            return result;
+        }
+    }
+}
