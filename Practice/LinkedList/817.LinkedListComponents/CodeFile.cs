f6c3a5386e511813bbbd0fe906da2378914281ac
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-02-02 20:58:10 -0500

    LinkedList: 817 Linked List components

---
+++ Practice/LinkedList/817.LinkedListComponents/CodeFile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.LinkedList._817.LinkedListComponents
+{
+    internal class CodeFile
+    {
+        public int NumComponents(ListNode head, int[] nums)
+        {
+            var count = 0;
+            var hashSet = new HashSet<int>(nums);
+            var connected = false;
+
+            while (head != null)
+            {
+                if (hashSet.Add(head.val))
+                {
+                    connected = false;
+                }
+                else if (!connected)
+                {
+                    count++;
+                    connected = true;
+                }
+
+                head = head.next;
+            }
+
+            return count;
+        }
+    }
+}
