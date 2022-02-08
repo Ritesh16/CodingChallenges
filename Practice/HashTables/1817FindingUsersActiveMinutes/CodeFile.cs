34e5f57e1353a414fd22b1c8abcfe984d98c1554
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-02-08 18:27:45 -0500

    HashTable: Problem 1817 & 535

---
+++ Practice/HashTables/1817FindingUsersActiveMinutes/CodeFile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.HashTables._1817FindingUsersActiveMinutes
+{
+    public class CodeFile
+    {
+        public int[] FindingUsersActiveMinutes(int[][] logs, int k)
+        {
+            var result = new int[k];
+            var map = new Dictionary<int, HashSet<int>>();
+            for (int i = 0; i < logs.Length; i++)
+            {
+                if (map.ContainsKey(logs[i][0]))
+                {
+                    map[logs[i][0]].Add(logs[i][1]);
+                }
+                else
+                {
+                    var hashSet = new HashSet<int>();
+                    hashSet.Add(logs[i][1]);
+                    map.Add(logs[i][0], hashSet);
+                }
+            }
+
+            foreach (var elem in map)
+                result[elem.Value.Count - 1]++; // index = hashSet.Count - 1
+            return result;
+        }
+    }
+}
---
+++ Practice/HashTables/535.EncodeDecodTinyURL/CodeFile.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.HashTables._535.EncodeDecodTinyURL
+{
+    public class CodeFile
+    {
+        Dictionary<string, string> urltoKey = new Dictionary<string, string>();
+        Dictionary<string, string> keytoUrl = new Dictionary<string, string>();
+        const string KeyCharacterSet = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        const string urlPrefix = "http://tinyurl.com/";
+
+        public string encode(string longUrl)
+        {
+            var key = string.Empty;
+            var random = new Random();
+            if (!urltoKey.TryGetValue(longUrl, out key))
+            {
+                while (true)
+                {
+                    var builder = new StringBuilder();
+                    for (int i = 0; i < 6; i++)
+                    {
+                        int index = random.Next(0, KeyCharacterSet.Length);
+                        builder.Append(KeyCharacterSet[index]);
+                    }
+
+                    key = builder.ToString();
+                    if (!keytoUrl.ContainsKey(key))
+                    {
+                        keytoUrl.Add(key, longUrl);
+                        urltoKey.Add(longUrl, key);
+                        break;
+                    }
+                }
+            }
+
+            return urlPrefix + key;
+
+        }
+
+        public string decode(string shortUrl)
+        {
+            string key = shortUrl.Replace(urlPrefix, string.Empty);
+            return keytoUrl[key];
+        }
+    }
+}
