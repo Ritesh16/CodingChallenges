4f8e5eadbef4ca3b43eedbe441064d58382da17c
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-02-14 21:41:07 -0500

    Linked List: 725 Split Linked list in parts

---
+++ Practice/LinkedList/725.SplitLinkedListinParts/CodeFile-Fast.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.LinkedList._725.SplitLinkedListinParts
+{
+    public class CodeFile_Fast
+    {
+        public ListNode[] SplitListToParts(ListNode head, int k)
+        {
+            var result = new ListNode[k];
+            var list = new List<ListNode>();
+
+            while (head != null)
+            {
+                list.Add(head);
+                head = head.next;
+            }
+
+            var remainder = list.Count % k;
+            var width = list.Count / k;
+            var i = 0;
+
+            for (int j = 0; j < result.Length; j++)
+            {
+                if (j >= list.Count)
+                    break;
+
+                result[j] = list[i];
+
+                if (j < remainder)
+                {
+                    i += width + 1;
+                }
+                else
+                {
+                    i += width;
+                }
+
+                list[i - 1].next = null;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Practice/LinkedList/725.SplitLinkedListinParts/CodeFile.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.LinkedList._725.SplitLinkedListinParts
+{
+    public class CodeFile
+    {
+        public ListNode[] SplitListToParts(ListNode head, int k)
+        {
+            if (head == null) return new ListNode[k];
+
+            var length = GetLength(head);
+            var parts = length / k;
+
+            var array = new int[k];
+            for (int i = 0; i < k; i++)
+            {
+                array[i] = parts;
+            }
+
+
+            var remainder = length % k;
+            for (int i = 0; i < remainder; i++)
+            {
+                array[i]++;
+            }
+
+
+            var result = new ListNode[k];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                var total = array[i];
+                var counter = 0;
+                ListNode node = head;
+                var prev = node;
+                while (counter < total)
+                {
+                    counter++;
+                    prev = node;
+                    node = node.next;
+                }
+
+                if (prev != null)
+                {
+                    prev.next = null;
+                }
+
+                result[i] = head;
+                head = node;
+            }
+
+            return result;
+        }
+
+        public int GetLength(ListNode head)
+        {
+            var count = 0;
+            var root = head;
+            while (root != null)
+            {
+                count++;
+                root = root.next;
+            }
+
+            return count;
+        }
+    }
+}
