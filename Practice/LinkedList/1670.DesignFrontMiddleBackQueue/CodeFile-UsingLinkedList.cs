8f8da7d83c0337b256702991b230f628ffa3488b
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-02-15 19:30:02 -0500

    Linked List: 1670 Design front middle back deqeue

---
+++ Practice/LinkedList/1670.DesignFrontMiddleBackQueue/CodeFile-UsingLinkedList.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.LinkedList._1670.DesignFrontMiddleBackQueue
+{
+    public class FrontMiddleBackQueue
+    {
+        ListNode head;
+        ListNode tail;
+        int count = 0;
+        public FrontMiddleBackQueue()
+        {
+            head = new ListNode();
+        }
+
+        public void PushFront(int val)
+        {
+            var node = new ListNode();
+            node.val = val;
+
+            count++;
+            if (count == 1)
+            {
+                tail = node;
+                head = node;
+            }
+            else
+            {
+                var temp = head;
+                node.next = temp;
+                head = node;
+            }
+        }
+
+        public void PushMiddle(int val)
+        {
+            if (count <= 1)
+            {
+                PushFront(val);
+                return;
+            }
+
+            var root = head;
+            var mid = count / 2;
+            var i = 0;
+            while (i < mid - 1)
+            {
+                i++;
+                root = root.next;
+            }
+
+            var node = new ListNode(val);
+            var temp = root.next;
+            root.next = node;
+            node.next = temp;
+            count++;
+        }
+
+        public void PushBack(int val)
+        {
+            var node = new ListNode();
+            node.val = val;
+
+            count++;
+            if (count == 1)
+            {
+                tail = node;
+                head = node;
+            }
+            else
+            {
+                var temp = tail;
+                temp.next = node;
+                tail = node;
+            }
+        }
+
+        public int PopFront()
+        {
+            if (count == 0) return -1;
+
+            count--;
+            var temp = head;
+            head = head.next;
+
+            return temp.val;
+        }
+
+        public int PopMiddle()
+        {
+            if (count == 0) return -1;
+            if (count <= 2)
+            {
+                return PopFront();
+            }
+
+            var root = head;
+            var mid = (count - 1) / 2;
+
+
+            var i = 0;
+            while (i < mid - 1)
+            {
+                i++;
+                root = root.next;
+            }
+
+            var temp = root.next.val;
+            root.next = root.next.next;
+            count--;
+
+            return temp;
+        }
+
+        public int PopBack()
+        {
+            if (count == 0) return -1;
+
+            var i = 0;
+            var root = head;
+            while (i < count - 2)
+            {
+                root = root.next;
+                i++;
+            }
+
+            var val = tail.val;
+            root.next = null;
+            tail = root;
+            count--;
+            return val;
+        }
+    }
+}
---
+++ Practice/LinkedList/1670.DesignFrontMiddleBackQueue/CodeFile-UsingList.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.LinkedList._1670.DesignFrontMiddleBackQueue
+{
+    public class FrontMiddleBackQueueList
+    {
+        List<int> list;
+        public FrontMiddleBackQueueList()
+        {
+            list = new List<int>();
+        }
+
+        public void PushFront(int val)
+        {
+            list.Insert(0, val);
+        }
+
+        public void PushMiddle(int val)
+        {
+            var mid = GetMiddle(false);
+            list.Insert(mid, val);
+        }
+
+        public void PushBack(int val)
+        {
+            list.Add(val);
+        }
+
+        public int PopFront()
+        {
+            if (list.Count == 0) return -1;
+
+            var val = list[0];
+            list.RemoveAt(0);
+
+            return val;
+        }
+
+        public int PopMiddle()
+        {
+            if (list.Count == 0) return -1;
+
+            var mid = GetMiddle(true);
+            var val = list[mid];
+            list.RemoveAt(mid);
+            return val;
+        }
+
+        public int PopBack()
+        {
+            if (list.Count == 0) return -1;
+            var last = list.Count - 1;
+            var val = list[last];
+            list.RemoveAt(last);
+            return val;
+        }
+
+        private int GetMiddle(bool lower)
+        {
+            var offset = lower ? 1 : 0;
+            var mid = (list.Count - offset) / 2;
+
+            if (mid < 0)
+                mid = 0;
+            return mid;
+        }
+    }
+
+}
