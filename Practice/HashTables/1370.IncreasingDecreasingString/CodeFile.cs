7d233111d2f7c927466c31486745acdb88863ebf
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-02-22 22:51:30 -0500

    Hashtable : 1370 Increasing Decreasing String

---
+++ Practice/HashTables/1370.IncreasingDecreasingString/CodeFile.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.HashTables._1370.IncreasingDecreasingString
+{
+    public class CodeFile
+    {
+        public string SortString(string s)
+        {
+            int[] counts = new int[26];
+            foreach (var c in s)
+            {
+                counts[c - 'a']++;
+            }
+
+            StringBuilder sb = new StringBuilder(s.Length);
+            int direction = 1;
+            int idx = 0;
+
+            while (sb.Length != s.Length)
+            {
+                if (idx == 26 || idx == -1)
+                {
+                    direction = -direction;
+                    idx += direction;
+                }
+
+                if (counts[idx] > 0)
+                {
+                    counts[idx]--;
+                    sb.Append((char)(idx + 'a'));
+                }
+
+                idx += direction;
+            }
+
+            return sb.ToString();
+        }
+    }
+}
