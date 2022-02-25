6303ffb07748adbbf0695c1da92d493105d6dce6
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-02-24 21:35:21 -0500

    LinkedList: 218 Merge nodes in between zeros

---
+++ Practice/LinkedList/2181.MergeNodesBetweenZeros/CodeFile-Fast.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.LinkedList._2181.MergeNodesBetweenZeros
+{
+    public class CodeFile_Fast
+    {
+        public ListNode MergeNodes(ListNode head)
+        {
+            int sum = 0;
+            var temp = head;
+            var res = head;
+
+            while (temp != null)
+            {
+                if (temp.val != 0)
+                {
+                    sum += temp.val;
+                    temp = temp.next;
+                }
+                else
+                {
+                    res.val = sum;
+                    res.next = temp.next;
+
+                    temp = res.next;
+                    res = res.next;
+                    sum = 0;
+                }
+            }
+
+            return head.next;
+        }
+    }
+}
---
+++ Practice/LinkedList/2181.MergeNodesBetweenZeros/CodeFile.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.LinkedList._2181.MergeNodesBetweenZeros
+{
+    public class CodeFile
+    {
+        public ListNode MergeNodes(ListNode head)
+        {
+            var sum = 0;
+            var root = head;
+            ListNode result = null;
+            root = root.next;
+            ListNode previous = null;
+            while (root != null)
+            {
+                if (root.val != 0)
+                {
+                    sum += root.val;
+                }
+                else
+                {
+                    var node = new ListNode(sum);
+
+                    if (result == null)
+                    {
+                        result = node;
+                        previous = result;
+                    }
+                    else
+                    {
+                        previous.next = node;
+                        previous = previous.next;
+                    }
+
+                    sum = 0;
+                }
+
+                root = root.next;
+            }
+
+            return result;
+        }
+    }
+}
