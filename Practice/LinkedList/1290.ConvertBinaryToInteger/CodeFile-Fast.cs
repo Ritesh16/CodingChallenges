e7396084530fa52beb0e57c4e0925cec6cb8b221
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-02-25 08:27:56 -0500

    Linked List: Updating solutions for 876 & 1290

---
+++ Practice/LinkedList/1290.ConvertBinaryToInteger/CodeFile-Fast.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.LinkedList.ConvertBinaryToInteger
+{
+    public class CodeFile_Fast
+    {
+        public int GetDecimalValue(ListNode head)
+        {
+            var result = head.val;
+            while (head.next != null)
+            {
+                result = 2 * result + head.next.val;
+                head = head.next;
+            }
+
+            return result;
+        }
+    }
+}
--- Practice/LinkedList/876.MiddleOfLinkedList/CodeFile-Pointers.cs
+++ Practice/LinkedList/876.MiddleOfLinkedList/CodeFile-Pointers.cs
@@ -11,7 +11,7 @@ namespace Practice.LinkedList.MiddleOfLinkedList
             ListNode slow = head;
             ListNode fast = head;
 
-            while(fast!=null && fast.next != null)
+            while (fast != null && fast.next != null)
             {
                 slow = slow.next;
                 fast = fast.next.next;
