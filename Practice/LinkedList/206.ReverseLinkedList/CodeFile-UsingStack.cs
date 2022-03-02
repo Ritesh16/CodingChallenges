2f05ebcd20b1eec4f1d71feeac4bfd65253dc436
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-03-01 21:53:38 -0500

    Linked List : 206 Reverse Linked List

---
+++ Practice/LinkedList/206.ReverseLinkedList/CodeFile-UsingStack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.LinkedList.ReverseLinkedList
+{
+    public class CodeFile_UsingStack
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            var stack = new Stack<ListNode>();
+            var resultNode = new ListNode(int.MinValue);
+            var node = resultNode;
+
+            while (head != null)
+            {
+                if (head.val != int.MinValue)
+                {
+                    stack.Push(head);
+                    head = head.next;
+                }
+            }
+
+            while (stack.Count > 0)
+            {
+                node.next = stack.Peek();
+                node = node.next;
+                stack.Pop();
+            }
+
+            node.next = null;
+            return resultNode.next;
+        }
+    }
+}
