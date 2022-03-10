298205c55762cb47908aaab0fe39bf3b10d016f8
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-03-10 15:10:17 -0500

    Arrays: 1436 Destination City

---
+++ Practice/Arrays/1436.DestinationCity/CodeFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practice.Arrays._1436.DestinationCity
+{
+    public class CodeFile
+    {
+        public string DestCity(IList<IList<string>> paths)
+        {
+            var result = string.Empty;
+            var map = new Dictionary<string, string>();
+            foreach (var path in paths)
+            {
+                map.Add(path[0], path[1]);
+            }
+
+            var start = map.First().Value;
+            result = start;
+            while (map.ContainsKey(start))
+            {
+                result = map[start];
+                start = map[start];
+            }
+
+            return result;
+        }
+    }
+}
