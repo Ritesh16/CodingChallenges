c222c273b89aabf625760cf8805933e0b6566895
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-03-11 12:15:53 -0500

    Linked & Array : Adding problem 2057 and 109

---
+++ Practice/Arrays/2057.SmallestIndexWithEqualValue/CodeFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Arrays._2057.SmallestIndexWithEqualValue
+{
+    public class CodeFile
+    {
+        public int SmallestEqual(int[] nums)
+        {
+            var result = -1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i % 10 == nums[i])
+                {
+                    result = i;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Practice/LinkedList/109.ConvertSortedListToBinaryTree/CodeFile.cs
@@ -0,0 +1,44 @@
+﻿using Practice.BinarySearchTree;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.LinkedList._109.ConvertSortedListToBinaryTree
+{
+    public class CodeFile
+    {
+        public TreeNode SortedListToBST(ListNode head)
+        {
+            if (head == null)
+            {
+                return null;
+            }
+
+            return ToBST(head, null);
+        }
+
+        public TreeNode ToBST(ListNode first, ListNode last)
+        {
+            if (first == last)
+            {
+                return null;
+            }
+
+            var slow = first;
+            var fast = first;
+            while (fast != last && fast.next != last)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+
+            TreeNode treeNode = new TreeNode(slow.val);
+            treeNode.left = ToBST(first, slow);
+            treeNode.right = ToBST(slow.next, last);
+
+
+            return treeNode;
+        }
+    }
+}
