5523216d7615117fd75a1a2e60daa7a5bc65d78a
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-03-23 14:46:35 -0400

    Arrays : 1725. Number Of Rectangles That Can Form The Largest Square

---
+++ Practice/Arrays/1725.RectanglesThatCanFormLargestSquare/CodeFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Arrays._1725.RectanglesThatCanFormLargestSquare
+{
+    public class CodeFile
+    {
+        public int CountGoodRectangles(int[][] rectangles)
+        {
+            var maxLen = 0;
+            var maxCnt = 0;
+
+            foreach (var rec in rectangles)
+            {
+                var min = Math.Min(rec[0], rec[1]);
+                if (min > maxLen)
+                {
+                    maxLen = min;
+                    maxCnt = 1;
+                }
+                else if (min == maxLen)
+                {
+                    maxCnt++;
+                }
+            }
+
+            return maxCnt;
+        }
+    }
+}
