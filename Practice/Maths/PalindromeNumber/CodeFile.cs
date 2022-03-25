a53ce83a64040162742c071911f3392dd483a086
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-03-24 21:37:55 -0400

    Maths:Plandrome Number

---
+++ Practice/Maths/PalindromeNumber/CodeFile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Maths.PalindromeNumber
+{
+    public class CodeFile
+    {
+        public bool IsPalindrome(int x)
+        {
+            var num = x;
+            var sum = 0;
+            var remainder = 0;
+
+            while (num > 0)
+            {
+                remainder = num % 10;
+                num = num / 10; ;
+                sum = sum * 10 + remainder;
+            }
+
+            return sum == x;
+        }
+    }
+}
