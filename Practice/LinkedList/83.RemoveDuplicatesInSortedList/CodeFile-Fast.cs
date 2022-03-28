93d84117d4881989ed0231a5836c2517e942dc5c
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-03-28 16:06:05 -0400

    Linked List: 83 Removing duplicates from sorted list

---
+++ Practice/LinkedList/83.RemoveDuplicatesInSortedList/CodeFile-Fast.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.LinkedList.RemoveDuplicatesInSortedList
+{
+    public class CodeFile_Fast
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            var curr = head;
+            if (head == null)
+            {
+                return head;
+            }
+
+            while (curr.next != null)
+            {
+                if (curr.val == curr.next.val)
+                {
+                    curr.next = curr.next.next;
+                }
+                else
+                {
+                    curr = curr.next;
+                }
+            }
+
+            return head;
+        }
+    }
+}
