f28578a9798e3057232292ae2e1e778355b4956a
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-04-01 09:43:08 -0400

    LinkedList: 2074- Updated solution

---
+++ Practice/LinkedList/2074.ReverseNodesinEvenLengthGroups/CodeFile-Fast.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.LinkedList._2074.ReverseNodesinEvenLengthGroups
+{
+    // Less Memory
+    public class CodeFile_Fast
+    {
+        public ListNode ReverseEvenLengthGroups(ListNode head)
+        {
+            if (head == null || head.next == null) return head;
+
+            var group = 1;
+            var count = 0;
+            var result = new ListNode(int.MinValue);
+            var current = result;
+            while (head != null)
+            {
+                var node = new ListNode(int.MinValue);
+                var c = node;
+                while (count < group && head != null)
+                {
+                    c.next = new ListNode(head.val);
+                    c = c.next;
+                    head = head.next;
+                    count++;
+                }
+
+                if (count % 2 == 0)
+                {
+                    node.next = ReverseList(node.next);
+                }
+
+                current.next = node.next;
+                while (current.next != null)
+                {
+                    current = current.next;
+                }
+
+                group++;
+                count = 0;
+            }
+
+            return result.next;
+        }
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode prev = null;
+            var current = head;
+            while (current != null)
+            {
+                var nextNode = current.next;
+                current.next = prev;
+                prev = current;
+                current = nextNode;
+            }
+
+            return prev;
+        }
+    }
+}
