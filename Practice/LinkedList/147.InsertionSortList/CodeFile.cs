f5b3128e1705e577f1de3f992ace33a244e03e43
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-04-06 16:15:45 -0400

    Linked List : 147. Insertion Sort List

---
+++ Practice/LinkedList/147.InsertionSortList/CodeFile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.LinkedList._147.InsertionSortList
+{
+    public class CodeFile
+    {
+        public ListNode InsertionSortList(ListNode head)
+        {
+            if (head == null || head.next == null) return head;
+
+
+            var list = head;
+            while (list.next != null)
+            {
+                var next = list.next;
+                var start = head;
+                while (start != next)
+                {
+                    if (start.val > next.val)
+                    {
+                        var temp = start.val;
+                        start.val = next.val;
+                        next.val = temp;
+                    }
+
+                    start = start.next;
+                }
+
+                list = list.next;
+            }
+
+            return head;
+        }
+    }
+}
