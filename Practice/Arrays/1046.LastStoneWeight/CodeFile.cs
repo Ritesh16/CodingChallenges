028cfbb34a5b21231d800d021003a2fca507de43
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-04-07 21:02:54 -0400

    Arrays:1046 Last Stone Weight

---
+++ Practice/Arrays/1046.LastStoneWeight/CodeFile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practice.Arrays._1046.LastStoneWeight
+{
+    public class CodeFile
+    {
+        public int LastStoneWeight(int[] stones)
+        {
+            var list = new List<int>(stones);
+
+            return FindLastStone(list);
+        }
+
+        private int FindLastStone(List<int> list)
+        {
+            if (list.Count == 0)
+                return 0;
+            else if (list.Count == 1)
+                return list[0];
+            else
+            {
+                var num1 = list.Max();
+                list.Remove(num1);
+
+                var num2 = list.Max();
+                list.Remove(num2);
+
+                if (num1 > num2)
+                {
+                    var dif = Math.Abs(num2 - num1);
+                    list.Add(dif);
+                }
+
+                return FindLastStone(list);
+            }
+
+
+        }
+    }
+}
