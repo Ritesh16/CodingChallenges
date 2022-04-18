23b38ca6fe763fd8b6168c438d499ccd2c957dd2
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-04-18 16:57:44 -0400

    Arrays : 1979 Find GCD

---
+++ Practice/Arrays/1979. FindGCDOfArray/CodeFile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Arrays._1979._FindGCDOfArray
+{
+    public class CodeFile
+    {
+        public int FindGCD(int[] nums)
+        {
+            Array.Sort(nums);
+            var min = nums[0];
+            var max = nums[nums.Length - 1];
+
+            for (int i = min; i > 1; i--)
+            {
+                if (min % i == 0 && max % i == 0)
+                {
+                    return i;
+                }
+            }
+
+            return 1;
+        }
+    }
+}
