7a91bb8a55ef8078a89bde4c7ecba18f65cc6cdd
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-04-25 21:43:54 -0400

    Arrays: 682 Baseball Game

---
+++ Practice/Arrays/682.BaseballGame/CodeFile.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Arrays._682.BaseballGame
+{
+    public class CodeFile
+    {
+        public int CalPoints(string[] ops)
+        {
+            var stack = new Stack<int>();
+
+            foreach (var item in ops)
+            {
+                if (item == "C")
+                {
+                    stack.Pop();
+                }
+                else if (item == "D")
+                {
+                    var last = Convert.ToInt32(stack.Peek());
+                    last = last * 2;
+                    stack.Push(last);
+                }
+                else if (item == "+")
+                {
+                    var num1 = stack.Pop();
+                    var num2 = stack.Pop();
+                    var num3 = num1 + num2;
+
+                    stack.Push(num2);
+                    stack.Push(num1);
+                    stack.Push(num3);
+
+                }
+                else
+                {
+                    var num = Convert.ToInt32(item);
+                    stack.Push(num);
+                }
+            }
+
+
+            return stack.Sum();
+        }
+    }
+}
