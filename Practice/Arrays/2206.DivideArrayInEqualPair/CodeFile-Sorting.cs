423cea80cb5ccfe570a6e25b56992fbd97bce457
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-04-28 10:33:06 -0400

    Adding fast solution for 2206

---
+++ Practice/Arrays/2206.DivideArrayInEqualPair/CodeFile-Sorting.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Practice.Arrays._2206.DivideArrayInEqualPair
+{
+    public class CodeFile_Sorting
+    {
+        public bool DivideArray(int[] nums)
+        {
+            var result = true;
+            Array.Sort(nums);
+
+            for (int i = 1; i < nums.Length; i = i + 2)
+            {
+                if (nums[i] != nums[i - 1])
+                {
+                    return false;
+                }
+            }
+
+            return result;
+        }
+    }
+}
