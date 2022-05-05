dfc7cdb21718eb8625f0c0f1d318663e3529a95e
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-05-04 21:08:24 -0400

    Sorting: 1859. Sorting the Sentence

---
+++ Practice/Sorting/1859.SortingtheSentence/CodeFile-SingleLoop.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Sorting._1859.SortingtheSentence
+{
+    public class CodeFile_SingleLoop
+    {
+        public string SortSentence(string s)
+        {
+            var inputArray = s.Split(' ');
+            var outputArray = new string[inputArray.Length];
+
+            foreach (var i in inputArray)
+            {
+                if (int.TryParse(i[i.Length - 1].ToString(), out int ind))
+                {
+                    if (ind >= 1)
+                    {
+                        outputArray[ind - 1] = i.Substring(0, i.Length - 1);
+                    }
+                }
+            }
+
+
+            var output = string.Join(' ', outputArray);
+
+
+            return output;
+        }
+    }
+}
---
+++ Practice/Sorting/1859.SortingtheSentence/CodeFile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Sorting._1859.SortingtheSentence
+{
+    public class CodeFile
+    {
+        public string SortSentence(string s)
+        {
+            var inputArray = s.Split(' ');
+            var outputArray = new string[inputArray.Length];
+
+            foreach (var item in inputArray)
+            {
+                foreach (var c in item)
+                {
+                    if (char.IsDigit(c))
+                    {
+                        var index = Convert.ToInt16(char.GetNumericValue(c));
+                        var word = item.Substring(0, item.Length - 1);
+                        outputArray[index - 1] = word;
+                    }
+                }
+            }
+
+
+            var output = string.Join(' ', outputArray);
+
+
+            return output;
+        }
+    }
+}
