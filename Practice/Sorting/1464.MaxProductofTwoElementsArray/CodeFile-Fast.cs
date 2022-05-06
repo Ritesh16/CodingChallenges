7f2d66dd0c7c7173dd03c580c60c58b759c85b46
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-05-05 21:46:52 -0400

    Sorting: 1464. Maximum Product of Two Elements in an Array

---
+++ Practice/Sorting/1464.MaxProductofTwoElementsArray/CodeFile-Fast.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Sorting._1464.MaxProductofTwoElementsArray
+{
+    public class CodeFile_Fast
+    {
+        public int MaxProduct(int[] nums)
+        {
+            int i = 0;
+
+            for (int k = 1; k < nums.Length; k++)
+            {
+                if (nums[k] > nums[i]) i = k;
+            }
+
+            int j = i == 0 ? 1 : 0;
+            for (int k = j + 1; k < nums.Length; k++)
+            {
+                if (nums[k] > nums[j] && k != i) j = k;
+            }
+            Console.WriteLine($"i = {i}, j = {j}");
+
+            return (nums[i] - 1) * (nums[j] - 1);
+        }
+    }
+}
---
+++ Practice/Sorting/1464.MaxProductofTwoElementsArray/CodeFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Sorting._1464.MaxProductofTwoElementsArray
+{
+    public class CodeFile
+    {
+        public int MaxProduct(int[] nums)
+        {
+            var result = 0;
+            if (nums.Length == 2)
+            {
+                result = (nums[0] - 1) * (nums[1] - 1);
+                return result;
+            }
+
+            Array.Sort(nums);
+            result = (nums[nums.Length - 1] - 1) * (nums[nums.Length - 2] - 1);
+
+            return result;
+        }
+    }
+}
