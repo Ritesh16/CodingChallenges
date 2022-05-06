c719f2cc3b9879df9565472e39a0a3ef876d9992
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-05-06 16:06:57 -0400

    Sorting: 2154 Keep Multiplying Found Values by Two

---
+++ Practice/Sorting/2154.KeepMultiplyingFoundValuesby2/CodeFile-Fast.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Sorting._2154.KeepMultiplyingFoundValuesby2
+{
+    public class CodeFile_Fast
+    {
+        public int FindFinalValue(int[] nums, int original)
+        {
+            int i = 0;
+            int j = nums.Length - 1;
+
+            while (i <= j)
+            {
+                if (nums[i] == original || nums[j] == original)
+                {
+                    original *= 2;
+                    i = -1;
+                    j = nums.Length;
+                }
+                i++;
+                j--;
+            }
+            return original;
+        }
+    }
+}
---
+++ Practice/Sorting/2154.KeepMultiplyingFoundValuesby2/CodeFile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Sorting._2154.KeepMultiplyingFoundValuesby2
+{
+    public class CodeFile
+    {
+        public int FindFinalValue(int[] nums, int original)
+        {
+            if(nums.Length > 1)
+            {
+                Array.Sort(nums);
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(original == nums[i])
+                {
+                    original = original * 2;
+                }
+            }
+
+            return original;
+        }
+    }
+}
