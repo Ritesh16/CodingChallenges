ce1991d4f46c92c8dde0e99a0beef8b9c4adbf3f
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-05-10 16:41:21 -0400

    Sorting: 1460. Make Two Arrays Equal by Reversing Sub-arrays

---
+++ Practice/Sorting/1460.MakeTwoArraysEqualByReversing/CodeFile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Sorting._1460.MakeTwoArraysEqualByReversing
+{
+    public class CodeFile
+    {
+        public bool CanBeEqual(int[] target, int[] arr)
+        {
+            Array.Sort(target);
+            Array.Sort(arr);
+
+            var i = 0;
+
+            while (i < arr.Length)
+            {
+                if (target[i] != arr[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
