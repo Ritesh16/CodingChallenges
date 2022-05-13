60c33ca77bcb29f57ad1f237ee8eef7490e2bc92
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-05-12 22:11:01 -0400

    Sorting: 1637. Widest Vertical Area Between Two Points Containing No Points

---
+++ Practice/Sorting/1637.WidestVerticalAreaBetween2Points/CodeFile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Sorting._1637.WidestVerticalAreaBetween2Points
+{
+    public class CodeFile
+    {
+        public int MaxWidthOfVerticalArea(int[][] points)
+        {
+            List<int> xPoints = new List<int>();
+
+            foreach (var item in points)
+            {
+                xPoints.Add(item[0]);
+            }
+
+            xPoints.Sort();
+            int max = 0;
+
+            for (int i = 1; i < xPoints.Count; i++)
+            {
+                max = Math.Max(max, xPoints[i] - xPoints[i - 1]);
+            }
+
+            return max;
+        }
+    }
+}
