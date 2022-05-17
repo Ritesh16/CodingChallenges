994dcfc2b252e5a1b6e5524603043dd573ac87cd
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-05-17 17:08:29 -0400

    Arrays: 1748 Sum of unique numbers

---
+++ Practice/Arrays/1748.SumofUniqueElements/CodeFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Arrays._1748.SumofUniqueElements
+{
+    public class CodeFile
+    {
+        public int SumOfUnique(int[] nums)
+        {
+            var result = 0;
+            var hashSet = new HashSet<int>();
+
+            foreach (var item in nums)
+            {
+                if (!hashSet.Contains(item))
+                {
+                    result += item;
+                }
+                else if(result > 0)
+                {
+                    result -= item;
+                }
+
+                hashSet.Add(item);
+            }
+
+            return result;
+        }
+    }
+}
