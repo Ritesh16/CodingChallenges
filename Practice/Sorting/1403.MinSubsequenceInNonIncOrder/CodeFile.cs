d285b4f0d3458f2c6f9b63f6909b11063bc832fd
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-05-23 12:58:08 -0400

    Sorting: 1403 Min subsequence

---
+++ Practice/Sorting/1403.MinSubsequenceInNonIncOrder/CodeFile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Sorting._1403.MinSubsequenceInNonIncOrder
+{
+    public class CodeFile
+    {
+        public IList<int> MinSubsequence(int[] nums)
+        {
+            var result = new List<int>();
+            Array.Sort(nums);
+
+            var sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+            }
+
+            var maxSum = 0;
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                maxSum += nums[i];
+                sum -= nums[i];
+                result.Add(nums[i]);
+                if (sum < maxSum)
+                {
+                    break;
+                }
+
+               
+            }
+
+            return result;
+        }
+    }
+}
