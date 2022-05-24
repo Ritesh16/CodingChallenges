cf5b50eecce12f6f44186976c019ec42965ba272
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-05-24 15:38:51 -0400

    Bit Manipulation: 260 Single Number lll

---
+++ Practice/BitManipulation/260.SingleNumberlII/CodeFile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.BitManipulation._260.SingleNumberlII
+{
+    public class CodeFile
+    {
+		public int[] SingleNumber(int[] nums)
+		{
+			var xor = nums.Aggregate(0, (x, y) => x ^ y);
+			int bit = 1, n1 = 0, n2 = 0;
+			while ((xor & 1) == 0)
+			{
+				bit <<= 1;
+				xor >>= 1;
+			}
+			foreach (var n in nums)
+			{
+				if ((n & bit) == 0)
+				{
+					n1 ^= n;
+				}
+				else
+				{
+					n2 ^= n;
+				}
+			}
+			return new[] { n1, n2 };
+		}
+	}
+}
