98a7800d2eea172fc2723bfe2692ca2a85628b03
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-05-26 20:21:08 -0400

    BitManipulation : 1720XRoed Array

---
+++ Practice/BitManipulation/1720.DecodeXORedArray/CodeFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.BitManipulation._1720.DecodeXORedArray
+{
+    public class CodeFile
+    {
+        public int[] Decode(int[] encoded, int first)
+        {
+            var result = new int[encoded.Length + 1];
+            result[0] = first;
+
+            for (int i = 1; i <= encoded.Length; i++)
+            {
+                result[i] = result[i - 1] ^ encoded[i - 1];
+            }
+
+            return result;
+        }
+    }
+}
