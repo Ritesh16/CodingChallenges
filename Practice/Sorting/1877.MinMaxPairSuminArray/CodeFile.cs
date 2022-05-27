e268152924f7c7acec8182b8cbcc690ee1b876ad
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-05-27 16:47:04 -0400

    Sorting: 1877. Minimize Maximum Pair Sum in Array

---
+++ Practice/Sorting/1877.MinMaxPairSuminArray/CodeFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Sorting._1877.MinMaxPairSuminArray
+{
+    public class CodeFile
+    {
+        public int MinPairSum(int[] nums)
+        {
+            Array.Sort(nums);
+            var max = 0;
+
+            for (int i = 0; i < nums.Length / 2; i++)
+            {
+                max = Math.Max(max, nums[i] + nums[nums.Length - 1 - i]);
+            }
+
+            return max;
+        }
+    }
+}
