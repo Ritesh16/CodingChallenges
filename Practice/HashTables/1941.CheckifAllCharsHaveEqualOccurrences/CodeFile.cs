4d6555b3ac897bb941bca32f41041b91476a0c2a
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-05-31 18:18:18 -0400

    HashTable: 1941. Check if All Characters Have Equal Number of Occurrences

---
+++ Practice/HashTables/1941.CheckifAllCharsHaveEqualOccurrences/CodeFile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.HashTables._1941.CheckifAllCharsHaveEqualOccurrences
+{
+    public class CodeFile
+    {
+        public bool AreOccurrencesEqual(string s)
+        {
+            var array = new int[26];
+            var m = 0;
+            foreach (var c in s)
+            {
+                var index = c - 'a';
+                array[index]++;
+
+                m = index;
+            }
+
+            for (int i = 0; i < 26; i++)
+            {
+                if (array[i] != 0 && array[i] != array[m])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
