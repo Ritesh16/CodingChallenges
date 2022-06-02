5e561715cfdf12ba6397ba940aa6eafc5e1538f9
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-06-01 21:43:17 -0400

    Sorting: 1305. All Elements in Two Binary Search Trees

---
+++ Practice/Sorting/1305.AllElementsin2BST/CodeFile-Fast.cs
@@ -0,0 +1,50 @@
+﻿using Practice.BinarySearchTree;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Sorting._1305.AllElementsin2BST
+{
+    public class CodeFile_Fast
+    {
+        public IList<int> GetAllElements(TreeNode root1, TreeNode root2)
+        {
+            var stack1 = new Stack<TreeNode>();
+            var stack2 = new Stack<TreeNode>();
+            var output = new List<int>();
+
+            while (root1 != null || root2 != null ||
+                  stack1.Count > 0 || stack2.Count > 0)
+            {
+                while (root1 != null)
+                {
+                    stack1.Push(root1);
+                    root1 = root1.left;
+                }
+
+                while (root2 != null)
+                {
+                    stack2.Push(root2);
+                    root2 = root2.left;
+                }
+
+                if (stack2.Count == 0 || stack1.Count > 0 &&
+                   stack1.Peek().val <= stack2.Peek().val)
+                {
+                    root1 = stack1.Pop();
+                    output.Add(root1.val);
+                    root1 = root1.right;
+                }
+                else
+                {
+                    root2 = stack2.Pop();
+                    output.Add(root2.val);
+                    root2 = root2.right;
+                }
+
+            }
+
+            return output;
+        }
+    }
+}
---
+++ Practice/Sorting/1305.AllElementsin2BST/CodeFile.cs
@@ -0,0 +1,84 @@
+﻿using Practice.BinarySearchTree;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Sorting._1305.AllElementsin2BST
+{
+    public class CodeFile
+    {
+        public IList<int> GetAllElements(TreeNode root1, TreeNode root2)
+        {
+            List<int> list1;
+            List<int> list2;
+
+            if (root1 == null)
+            {
+                list1 = new List<int>();
+            }
+
+            if (root2 == null)
+            {
+                list2 = new List<int>();
+            }
+
+            list1 = InOrder(root1);
+            list2 = InOrder(root2);
+
+            var list = new List<int>();
+            var i = 0;
+            var j = 0;
+
+            while (i < list1.Count && j < list2.Count)
+            {
+                if (list1[i] > list2[j])
+                {
+                    list.Add(list2[j]);
+                    j++;
+                }
+                else
+                {
+                    list.Add(list1[i]);
+                    i++;
+                }
+            }
+
+            while (i < list1.Count)
+            {
+                list.Add(list1[i]);
+                i++;
+            }
+
+            while (j < list2.Count)
+            {
+                list.Add(list2[j]);
+                j++;
+            }
+
+            return list;
+        }
+
+        public List<int> InOrder(TreeNode root)
+        {
+            var node = root;
+            var stack = new Stack<TreeNode>();
+            var list = new List<int>();
+
+            while (node != null || stack.Count != 0)
+            {
+                while (node != null)
+                {
+                    stack.Push(node);
+                    node = node.left;
+                }
+
+                node = stack.Pop();
+                list.Add(node.val);
+
+                node = node.right;
+            }
+
+            return list;
+        }
+    }
+}
