2391ae72463de9e8b4463bde5ff93b64a6208d40
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-06-06 21:19:47 -0400

    Sorting: 1561 Maximum Number of Coins You Can Get

---
+++ Practice/Sorting/1561.MaximumNoofCoinsYouCanGet/CodeFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Sorting._1561.MaximumNoofCoinsYouCanGet
+{
+    public class CodeFile
+    {
+        public int MaxCoins(int[] piles)
+        {
+            Array.Sort(piles);
+
+            int res = 0;
+            for (int i = piles.Length / 3; i < piles.Length; i += 2)
+            {
+                res += piles[i];
+            }
+
+            return res;
+        }
+    }
+}
