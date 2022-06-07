3be5d8cde517279da4fdb5faae1c06a000e106d8
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-06-06 21:15:38 -0400

    Hashtable: 961 N-Repeated Element in Size 2N Array

---
+++ Practice/HashTables/961.N-RepeatedElementin2NArray/CodeFile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.HashTables._961.N_RepeatedElementin2NArray
+{
+    public class CodeFile
+    {
+        public int RepeatedNTimes(int[] nums)
+        {
+            var result = int.MinValue;
+            var map = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (map.ContainsKey(nums[i]))
+                {
+                    map[nums[i]]++;
+                    return nums[i];
+                }
+                else
+                {
+                    map.Add(nums[i], 1);
+                }
+            }
+
+            return result;
+        }
+    }
+}
