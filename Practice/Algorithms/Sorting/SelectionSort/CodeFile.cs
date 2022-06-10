48f44574c4d42912e3a00713e65bce4487728ae3
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-06-10 16:43:39 -0400

    Adding code for selection sort

---
+++ Practice/Algorithms/Sorting/SelectionSort/CodeFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Algorithms.Sorting.SelectionSort
+{
+    public class CodeFile
+    {
+        public int[] Sort(int[] input)
+        {
+            var temp = 0;
+            var smallest = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                smallest = i;
+                for (int j = i + 1; j < input.Length; j++)
+                {
+                    if (input[j] < input[smallest])
+                        smallest = j;
+                }
+
+                temp = input[smallest];
+                input[smallest] = input[i];
+                input[i] = temp;
+            }
+
+            return input;
+        }
+    }
+}
