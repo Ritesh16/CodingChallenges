dae621191f08b578d869dd98bfa35553bc49391f
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-06-21 19:24:26 -0400

    Sorting: 1710 Maximum Units on a Truck

---
+++ Practice/Sorting/1710.MaxUnitsonTruck/CodeFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Sorting._1710.MaxUnitsonTruck
+{
+    public class CodeFile
+    {
+        public int MaximumUnits(int[][] boxTypes, int truckSize)
+        {
+
+            Array.Sort(boxTypes, (a, b) => { return b[1] - a[1]; });
+
+            int count = 0;
+
+            foreach (var type in boxTypes)
+            {
+                int num = Math.Min(type[0], truckSize);
+                count += num * type[1];
+
+                if (truckSize < num)
+                    break;
+
+                truckSize -= num;
+            }
+
+            return count;
+        }
+    }
+}
