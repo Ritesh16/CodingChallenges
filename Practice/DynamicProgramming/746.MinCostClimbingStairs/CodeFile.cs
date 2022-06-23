dcc8bc85cbb72b5e7ac13ed69efe4ac7ea8c112c
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-06-23 17:39:54 -0400

    Dynamic Programming : 746 Min Cost Climbing Stairs

---
+++ Practice/DynamicProgramming/746.MinCostClimbingStairs/CodeFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.DynamicProgramming._746.MinCostClimbingStairs
+{
+    public class CodeFile
+    {
+        public int MinCostClimbingStairs(int[] cost)
+        {
+            var prevPrev = cost[0];
+            var prev = cost[1];
+
+            for (int i = 2; i < cost.Length; i++)
+            {
+                var currCost = Math.Min(prev, prevPrev) + cost[i];
+                prevPrev = prev;
+                prev = currCost;
+            }
+
+            return Math.Min(prev, prevPrev);
+        }
+    }
+}
