ee61a8dad3244383d49e40e88fb6f06fc31062a9
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-06-24 18:41:33 -0400

    Maths: Adding problems- 1342 & 2235

---
+++ Practice/Maths/1342.StepstoReduceNumbertoZero/CodeFile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Maths._1342.StepstoReduceNumbertoZero
+{
+    public class CodeFile
+    {
+        public int NumberOfSteps(int num)
+        {
+            var steps = 0;
+            while (num > 0)
+            {
+                if (num % 2 == 0)
+                {
+                    num = num / 2;
+                }
+                else
+                {
+                    num--;
+                }
+
+                steps++;
+            }
+
+            return steps;
+        }
+    }
+}
---
+++ Practice/Maths/2235.AddTwoIntegers/CodeFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Maths._2235.AddTwoIntegers
+{
+    public class CodeFile
+    {
+        public int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
