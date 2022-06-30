1574809ad595d01dfae62455eafb84da95652bca
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-06-29 21:48:50 -0400

    Linked List : 92 ReverseLinkedlistll & Mergesort

---
+++ Practice/Algorithms/Sorting/MergeSort/CodeFile.cs
@@ -0,0 +1,66 @@
+﻿namespace Practice.Algorithms.Sorting.MergeSort
+{
+    public class CodeFile
+    {
+        public int[] SortArray(int[] array)
+        {
+            return MergeSort(array, 0, array.Length - 1);
+        }
+
+
+        private int[] MergeSort(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                var mid = (right - left) / 2 + left;
+
+                var array1 = MergeSort(array, left, mid);
+                var array2 = MergeSort(array, mid + 1, right);
+
+                return Merge(array1, array2);
+            }
+
+            return new int[] { array[left] };
+        }
+
+        private int[] Merge(int[] array1, int[] array2)
+        {
+            var array = new int[array1.Length + array2.Length];
+            var i = 0;
+            var j = 0;
+
+            var index = 0;
+            while (i < array1.Length && j < array2.Length)
+            {
+                if (array1[i] < array2[j])
+                {
+                    array[index] = array1[i];
+                    i++;
+                }
+                else
+                {
+                    array[index] = array2[j];
+                    j++;
+                }
+                index++;
+            }
+
+            while (i < array1.Length)
+            {
+                array[index] = array1[i];
+                i++;
+                index++;
+            }
+
+            while (j < array2.Length)
+            {
+                array[index] = array2[j];
+                j++;
+                index++;
+            }
+
+
+            return array;
+        }
+    }
+}
---
+++ Practice/LinkedList/92.ReverseLinkedListll/CodeFile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.LinkedList._92.ReverseLinkedListll
+{
+    public class CodeFile
+    {
+        public ListNode ReverseBetween(ListNode head, int left, int right)
+        {
+            if (head == null || left == right)
+            {
+                return head;
+            }
+
+            var dummy = new ListNode(int.MinValue);
+            dummy.next = head;
+
+            var previousNodeBeforeReverse = dummy;
+            ListNode current = head;
+            for (int i = 1; i < left; i++)
+            {
+                previousNodeBeforeReverse = current;
+                current = current.next;
+            }
+
+
+            ListNode prevNode = null;
+
+            for (int i = left; i <= right; i++)
+            {
+                var temp = current.next;
+                current.next = prevNode;
+                prevNode = current;
+                current = temp;
+            }
+
+            previousNodeBeforeReverse.next.next = current;
+            previousNodeBeforeReverse.next = prevNode;
+
+            return dummy.next;
+        }
+    }
+}
