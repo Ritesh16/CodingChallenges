f49124da837f8a0932205c11f9eaf8d0501dc04c
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-07-05 20:54:02 -0400

    Maths : 1688. Count of Matches in Tournament

---
+++ Practice/Maths/1688.CountofMatchesinTournament/CodeFile.cs
@@ -0,0 +1,10 @@
+﻿namespace Practice.Maths._1688.CountofMatchesinTournament
+{
+    public class CodeFile
+    {
+        public int NumberOfMatches(int n)
+        {
+            return n - 1;
+        }
+    }
+}
