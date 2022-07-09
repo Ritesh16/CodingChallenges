9d5096e396d1e2b67128b9340bf8b8957e659ec0
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-07-08 22:10:46 -0400

    Binary Tree : 617 Merge Binary trees

---
+++ Practice/BinaryTree/617.MergeTwoBinaryTrees/CodeFile.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.BinaryTree._617.MergeTwoBinaryTrees
+{
+    public class CodeFile
+    {
+        public TreeNode MergeTrees(TreeNode root1, TreeNode root2)
+        {
+            if (root1 == null)
+                return root2;
+
+
+            var stack = new Stack<TreeNode[]>();
+            stack.Push(new TreeNode[] { root1, root2 });
+
+            while (stack.Count > 0)
+            {
+                var t = stack.Pop();
+                if (t[0] == null || t[1] == null)
+                    continue;
+
+                t[0].val += t[1].val;
+
+                if (t[0].left == null)
+                {
+                    t[0].left = t[1].left;
+                }
+                else
+                {
+                    stack.Push(new TreeNode[] { t[0].left, t[1].left });
+                }
+
+                if (t[0].right == null)
+                {
+                    t[0].right = t[1].right;
+                }
+                else
+                {
+                    stack.Push(new TreeNode[] { t[0].right, t[1].right });
+                }
+            }
+
+            return root1;
+        }
+    }
+}
