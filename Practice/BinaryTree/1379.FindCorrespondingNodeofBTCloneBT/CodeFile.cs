1cbae998291524803ed7c626670a471c07b5ce94
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-07-12 18:08:39 -0400

    Binary Tree : 1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree

---
+++ Practice/BinaryTree/1379.FindCorrespondingNodeofBTCloneBT/CodeFile.cs
@@ -0,0 +1,31 @@
+﻿namespace Practice.BinaryTree._1379.FindCorrespondingNodeofBTCloneBT
+{
+    public class CodeFile
+    {
+        TreeNode answer;
+        TreeNode target;
+        public TreeNode GetTargetCopy(TreeNode original, TreeNode cloned, TreeNode target)
+        {
+            this.target = target;
+            PreOrder(original, cloned);
+
+            return answer;
+        }
+
+        public void PreOrder(TreeNode original, TreeNode cloned)
+        {
+            if (original != null)
+            {
+                if (original == target)
+                {
+                    answer = cloned;
+                    return;
+                }
+
+                PreOrder(original.left, cloned.left);
+
+                PreOrder(original.right, cloned.right);
+            }
+        }
+    }
+}
