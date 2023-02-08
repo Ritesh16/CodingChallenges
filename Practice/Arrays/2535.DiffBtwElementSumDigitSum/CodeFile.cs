0a0b08a8bdc52b6ec9ad13f24838e70d1464aea0
Ritesh16 <ritesh.sharma.cse@gmail.com>
2023-02-08 16:18:27 -0500

    Adding problem 2535 of arrays

---
+++ Practice/Arrays/2535.DiffBtwElementSumDigitSum/CodeFile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Arrays._2535.DiffBtwElementSumDigitSum
+{
+    internal class CodeFile
+    {
+        public int DifferenceOfSum(int[] nums)
+        {
+            int result = 0;
+            int sum1 = 0;
+            int sum2 = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] >= 10)
+                {
+                    sum1 += nums[i];
+                    var k = nums[i];
+                    while (k > 0)
+                    {
+                        sum2 += (k % 10);
+                        k /= 10;
+                    }
+                }
+            }
+
+            result = Math.Abs(sum1 - sum2);
+            return result;
+        }
+    }
+}
---
+++ Practice/Arrays/2535.DiffBtwElementSumDigitSum/CodeFile_Slow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Arrays._2535.DiffBtwElementSumDigitSum
+{
+    internal class CodeFile_Slow
+    {
+        public int DifferenceOfSum(int[] nums)
+        {
+            int sum = 0;
+            foreach (int num in nums)
+            {
+                sum += num;
+                foreach (char ch in num.ToString())
+                    sum -= (ch - '0');
+            }
+            return sum;
+        }
+    }
+}
