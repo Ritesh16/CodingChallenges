fc22d9868c9d45607ea90a2c48b98843991fe7ef
Ritesh16 <ritesh.sharma.cse@gmail.com>
2023-02-14 21:48:28 -0500

    Adding 2367. Number of Arithmetic Triplets

---
+++ Practice/Arrays/2367.NumberofArithmeticTriplets/CodeFile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Arrays._2367.NumberofArithmeticTriplets
+{
+    public class CodeFile
+    {
+        public int ArithmeticTriplets(int[] nums, int diff)
+        {
+            var hashMap = new HashSet<int>();
+            var result = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                hashMap.Add(nums[i]);
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var num1 = nums[i] + diff;
+                var num2 = num1 + diff;
+
+                if (hashMap.Contains(num1) && hashMap.Contains(num2))
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
