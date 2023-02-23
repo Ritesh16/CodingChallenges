e0ebdbebd079560bf98add3c18b1bd08512e6e4d
Ritesh16 <ritesh.sharma.cse@gmail.com>
2023-02-23 08:22:41 -0500

    Arrays:2418 Sort the people

---
+++ Practice/Arrays/2418.SortthePeople/CodeFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practice.Arrays._2418.SortthePeople
+{
+    public class CodeFile
+    {
+        public string[] SortPeople(string[] names, int[] heights)
+        {
+            var map = new Dictionary<int, string>();
+            for (var i = 0; i < names.Length; i++)
+            {
+                map.Add(heights[i], names[i]);
+            }
+
+            Array.Sort(heights);
+
+            var output = new string[names.Length];
+            var index = 0;
+            for (int i = heights.Length - 1; i >= 0; i--)
+            {
+                output[index] = map[heights[i]];
+                index++;
+            }
+
+            return output;
+        }
+    }
+}
