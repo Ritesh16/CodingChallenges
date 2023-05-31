f6f51307ff825f526bbbf238fbcee9ad7e59ab63
Ritesh16 <ritesh.sharma.cse@gmail.com>
2023-05-30 20:57:49 -0400

    String : 709 ToLowerCase

---
+++ Practice/Strings/709.ToLowerCase/CodeFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice.Strings._709.ToLowerCase
+{
+    public class CodeFile
+    {
+        public string ToLowerCase(string s)
+        {
+            return s.ToLower();
+        }
+    }
+}
