c315a8a7e55b5a354cca2995418e2c943428fd6a
Ritesh16 <ritesh.sharma.cse@gmail.com>
2024-10-23 21:51:31 -0400

    Adding code for custom array/list

---
+++ ArrayDataType/Program.cs
@@ -0,0 +1,47 @@
+﻿using ArrayDataType.Types;
+
+var array = new ArrayDataType.Types.Array();
+
+array.Add(1);
+array.Add(2);
+array.Add(3);
+array.Add(4);
+
+array.Add(5);
+array.Add(6);
+array.Add(7);
+array.Add(8);
+array.Add(9);
+array.Add(10);
+array.Add(11);
+array.Add(12);
+
+Console.WriteLine("Items in array:");
+foreach (var item in array)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("******************************");
+
+array.Insert(0, 100);
+
+Console.WriteLine("Items in array after insert:");
+
+for (var i = 0; i< array.Length; i++)
+{
+    Console.WriteLine(array[i]);
+}
+
+Console.WriteLine($"Length : {array.Length}");
+Console.WriteLine("******************************");
+
+array.Delete(50);
+Console.WriteLine("Items in array after delete:");
+for (var i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i]);
+}
+
+Console.WriteLine($"Length : {array.Length}");
+Console.WriteLine("******************************");
---
+++ ArrayDataType/Types/Array.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayDataType.Types
+{
+    public class Array : IEnumerable<int>
+    {
+        public int Length { get; set; }
+
+        private int[] _data;
+        private int _size = 10;
+        private int _index = 0;
+        public Array()
+        {
+            Length = 0;
+            _data = new int[_size];
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                return _data[index];
+            }
+            set
+            {
+                if (index > Length)
+                {
+                    return;
+                }
+
+                _data[index] = value;
+            }
+        }
+
+        public void Add(int value)
+        {
+            if (Length == _size)
+            {
+                Resize();
+            }
+
+            _data[_index] = value;
+            _index++;
+            Length = _index;
+        }
+
+        public void Insert(int index, int value)
+        {
+            if (index > Length)
+            {
+                return;
+            }
+
+            for (int i = Length - 1; i >= index; i--)
+            {
+                _data[i + 1] = _data[i];
+            }
+
+            _data[index] = value;
+
+            Length++;
+        }
+
+        public void Delete(int index)
+        {
+            if (index > Length)
+            {
+                return;
+            }
+
+            for (int i = index; i < Length; i++)
+            {
+                _data[i] = _data[i + 1];
+            }
+
+            Length--;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (var i = 0; i < Length; i++)
+            {
+                yield return _data[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        private int[] Resize()
+        {
+            var newSize = _size * 2;
+            var data = new int[newSize];
+
+            for (int i = 0; i < _size; i++)
+            {
+                data[i] = _data[i];
+            }
+
+            _data = data;
+            _size = newSize;
+            return _data;
+        }
+    }
+}
