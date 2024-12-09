2f60f1a198220fac61b9b7b131dde6d1383e3e5c
Ritesh16 <ritesh.sharma.cse@gmail.com>
2024-12-08 21:22:59 -0500

    Adding code to coun the ndoes in Binary tree

---
+++ TreeDataType/BinaryTree/CountNodesInBinaryTree/Iterative/CodeFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeDataType.BinaryTree.CountNodesInBinaryTree.Iterative
+{
+    public class CodeFile<T>
+    {
+        public int Count(Node<T> root)
+        {
+            var stack = new Stack<Node<T>>();
+            var output = 0;
+            stack.Push(root);
+
+            while (stack.Count > 0)
+            {
+                var top = stack.Pop();
+                if (top != null)
+                {
+                    stack.Push(top.Right);
+                    stack.Push(top.Left);
+                    output++;
+                }
+            }
+
+            return output;
+        }
+    }
+}
---
+++ TreeDataType/BinaryTree/CountNodesInBinaryTree/Recursive/CodeFile.cs
@@ -0,0 +1,15 @@
+﻿namespace TreeDataType.BinaryTree.CountNodesInBinaryTree.Recursive
+{
+    public class CodeFile<T>
+    {
+        public int Count(Node<T> root)
+        {
+            if (root != null)
+            {
+                return Count(root.Left) + Count(root.Right) + 1;
+            }
+
+            return 0;
+        }
+    }
+}
