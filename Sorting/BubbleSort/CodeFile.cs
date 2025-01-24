08c3fba4efb7c4671f2ea60209898e07c482b8b0
Ritesh16 <ritesh.sharma.cse@gmail.com>
2025-01-23 21:22:21 -0500

    Adding new project for sorting

---
+++ Sorting/BubbleSort/CodeFile.cs
@@ -0,0 +1,24 @@
+﻿namespace Sorting.BubbleSort
+{
+    public class CodeFile
+    {
+        public int[] Sort(int[] input)
+        {
+            var temp = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j < input.Length - 1; j++)
+                {
+                    if (input[j] > input[j + 1])
+                    {
+                        temp = input[j + 1];
+                        input[j + 1] = input[j];
+                        input[j] = temp;
+                    }
+                }
+            }
+
+            return input;
+        }
+    }
+}
