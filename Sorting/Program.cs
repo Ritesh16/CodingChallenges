7efb03ea8f8da4da95aa9b8db627436b015552fb
Ritesh16 <ritesh.sharma.cse@gmail.com>
2025-01-24 21:52:42 -0500

    Adding code for selection sort

--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -1,2 +1,63 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var flag = true;
+while (flag)
+{
+    var array = new int[] { 5, 6, 2, 12, 8, 11, 1, 9 };
+    Console.WriteLine("Select the sorting algorithm. Sorting 5, 6, 2, 12, 8, 11, 1, 9");
+    Console.WriteLine("1. Bubble Sort");
+    Console.WriteLine("2. Insertion Sort");
+    Console.WriteLine("3. Selection Sort");
+    Console.WriteLine("9. Exit");
+
+    var choice = Convert.ToInt32(Console.ReadLine());
+
+    switch (choice)
+    {
+        case 1:
+            var bubbleSort = new Sorting.BubbleSort.CodeFile();
+            var sortedArray = bubbleSort.Sort(array);
+            Console.WriteLine("Sorted array using Bubble Sort:");
+            foreach (var item in sortedArray)
+            {
+                Console.Write(item + " ");
+            }
+            break;
+        case 2:
+            var insertionSort = new Sorting.InsertionSort.CodeFile();
+            var sortedArray2 = insertionSort.Sort(array);
+            Console.WriteLine("Sorted array using Insertion Sort:");
+            foreach (var item in sortedArray2)
+            {
+                Console.Write(item + " ");
+            }
+            break;
+        case 3:
+            var selectionSort = new Sorting.SelectionSort.CodeFile();
+            var sortedArray3 = selectionSort.Sort(array);
+            Console.WriteLine("Sorted array using Selection Sort:");
+            foreach (var item in sortedArray3)
+            {
+                Console.Write(item + " ");
+            }
+            break;
+        case 9:
+            flag = false;
+            break;
+
+        default:
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Invalid choice");
+            Console.ForegroundColor = ConsoleColor.White;
+            break;
+
+    }
+
+    Console.WriteLine();
+
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Exiting...");
+Console.ForegroundColor = ConsoleColor.White;
---
+++ Sorting/SelectionSort/CodeFile.cs
@@ -0,0 +1,27 @@
+﻿namespace Sorting.SelectionSort
+{
+    public class CodeFile
+    {
+        public int[] Sort(int[] nums)
+        {
+            var temp = 0;
+            var smallest = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                smallest = i;
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[j] < nums[smallest])
+                        smallest = j;
+                }
+
+                temp = nums[smallest];
+                nums[smallest] = nums[i];
+                nums[i] = temp;
+            }
+
+            return nums;
+        }
+    }
+}
