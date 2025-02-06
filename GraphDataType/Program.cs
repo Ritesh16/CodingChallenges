28e99750897e658c3dd03d9025ed10d42e620475
Ritesh16 <ritesh.sharma.cse@gmail.com>
2025-02-05 22:15:57 -0500

    Add code for BFS in a graph

---
+++ GraphDataType/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Graph");
+
+var graph = new GraphDataType.Traversal.BFS.CodeFile(7);
+graph.AddEdge(1, 2);
+graph.AddEdge(1, 3);
+graph.AddEdge(1, 4);
+graph.AddEdge(2, 3);
+graph.AddEdge(3, 4);
+graph.AddEdge(3, 5);
+graph.AddEdge(4, 5);
+graph.AddEdge(5, 6);
+graph.AddEdge(5, 7);
+
+graph.BFS(1);
+
+
+
+
+
---
+++ GraphDataType/Traversal/BFS/CodeFile.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphDataType.Traversal.BFS
+{
+    public class CodeFile
+    {
+        public int Vertices { get; set; }
+        private List<int>[] adjacentList;
+        public CodeFile(int vertices)
+        {
+            Vertices = vertices + 1;
+
+            adjacentList = new List<int>[Vertices];
+            for (int i = 0; i < Vertices; i++)
+            {
+                adjacentList[i] = new List<int>();
+            }
+        }
+
+        public void AddEdge(int source, int destination)
+        {
+            adjacentList[source].Add(destination);
+        }
+
+        public void BFS(int start)
+        {
+            bool[] visited = new bool[Vertices];
+            var queue = new Queue<int>();
+
+            visited[start] = true;
+            queue.Enqueue(start);
+
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+                Console.WriteLine(current);
+
+                foreach (var neighbour in adjacentList[current])
+                {
+                    if (!visited[neighbour])
+                    {
+                        visited[neighbour] = true;
+                        queue.Enqueue(neighbour);
+                    }
+                }
+            }
+        }
+    }
+}
