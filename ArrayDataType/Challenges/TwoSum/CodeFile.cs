728af8b7f27230d2d70b178ece9d4b7ab94079e8
Ritesh16 <ritesh.sharma.cse@gmail.com>
2025-02-22 21:26:08 -0500

    Adding problem of Two Sum

---
+++ ArrayDataType/Challenges/TwoSum/CodeFile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayDataType.Challenges.TwoSum
+{
+    public class CodeFile
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var output = new int[2];
+            var map = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var secondNumber = target - nums[i];
+                if (map.ContainsKey(nums[i]))
+                {
+                    output[0] = map[nums[i]];
+                    output[1] = i;
+                    break;
+                }
+
+                map.Add(secondNumber, i);
+            }
+
+
+            return output;
+        }
+    }
+}
