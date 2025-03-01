de5cd4e539b2073340c6222c9df63a6b8ad19469
Ritesh16 <ritesh.sharma.cse@gmail.com>
2025-02-28 21:08:18 -0500

    Added code for valid parentheses Leetcode #20

---
+++ StackDataType/Challenges/ValidParentheses/CodeFile.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackDataType.Challenges.ValidParentheses
+{
+    public class CodeFile
+    {
+        public bool IsValid(string s)
+        {
+            var result = false;
+            if (string.IsNullOrEmpty(s))
+            {
+                return result;
+            }
+
+            var stack = new Stack<char>();
+            foreach (var c in s)
+            {
+                var num = (int)c;
+                if (num == 91 || num == 123 || num == 40)
+                {
+                    stack.Push(c);
+                }
+                else 
+                {
+                    if(stack.Count == 0)
+                    {
+                        return result;
+                    }
+
+                    if ((num == 41 & stack.Peek() == 40) ||
+                        (num == 93 & stack.Peek() == 91) ||
+                        (num == 125 & stack.Peek() == 123))
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                
+            }
+
+            if(stack.Count == 0)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+    }
+}
--- StackDataType/Program.cs
+++ StackDataType/Program.cs
@@ -1,18 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
-StackDataType.Types.Interfaces.IStack<int> stack = new StackDataType.Types.StackUsingLinkedList();
-stack.Push(1);
-stack.Push(2);
-stack.Push(3);
-stack.Push(4);
-stack.Push(5);
-stack.Push(6);
-Console.WriteLine($"Size: {stack.Count}");
+//Console.WriteLine("Hello, World!");
+//StackDataType.Types.Interfaces.IStack<int> stack = new StackDataType.Types.StackUsingLinkedList();
+//stack.Push(1);
+//stack.Push(2);
+//stack.Push(3);
+//stack.Push(4);
+//stack.Push(5);
+//stack.Push(6);
+//Console.WriteLine($"Size: {stack.Count}");
 
-var top = stack.Peek();
+//var top = stack.Peek();
 
-Console.WriteLine($"Top : {top}");
+//Console.WriteLine($"Top : {top}");
 
-var pop = stack.Pop();
-Console.WriteLine($"Pop : {pop} , Size: {stack.Count}");
+//var pop = stack.Pop();
+//Console.WriteLine($"Pop : {pop} , Size: {stack.Count}");
+
+using StackDataType.Challenges.ValidParentheses;
+
+var ob = new CodeFile();
+var input = "(])";
+
+
+if(ob.IsValid(input))
+{
+    Console.WriteLine("Valid");
+}
+else
+{
+    Console.WriteLine("Invalid");
+}
