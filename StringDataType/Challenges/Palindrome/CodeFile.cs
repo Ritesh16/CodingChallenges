ef6a274e06e8a0651a9c1245364ee6fa5dafb245
Ritesh16 <ritesh.sharma.cse@gmail.com>
2025-03-20 21:47:55 -0400

    Adding code for string problem

---
+++ StringDataType/Challenges/Palindrome/CodeFile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringDataType.Challenges.Palindrome
+{
+    public class CodeFile
+    {
+        public bool IsPalindrome(string s)
+        {
+            var right = s.Length - 1;
+            for (int left = 0; left < s.Length; left++)
+            {
+                while (left < right && !char.IsLetterOrDigit(s[left]))
+                {
+                    left++;
+                }
+
+                while (left < right && !char.IsLetterOrDigit(s[right]))
+                {
+                    right--;
+                }
+
+                if (char.ToLower(s[left]) != char.ToLower(s[right]))
+                {
+                    return false;
+                }
+
+                if (left >= right)
+                {
+                    break;
+                }
+
+                right--;
+
+            }
+
+            return true;
+        }
+    }
+}
