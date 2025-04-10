ef4dab9ff9196715e088e818262b0d998c695387
Ritesh16 <ritesh.sharma.cse@gmail.com>
2025-04-09 21:24:52 -0400

    Adding problem 6 invert a binary tree

---
+++ TreeDataType/Challenges/InvertBinaryTree/CodeFile_Recursive.cs
@@ -0,0 +1,19 @@
+﻿namespace TreeDataType.Challenges.InvertBinaryTree
+{
+    public class CodeFile_Recursive
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null) return null;
+
+            var temp = root.Left;
+            root.Left = root.Right;
+            root.Right = temp;
+
+            InvertTree(root.Left);
+            InvertTree(root.Right);
+
+            return root;
+        }
+    }
+}
---
+++ TreeDataType/TreeNode.cs
@@ -0,0 +1,16 @@
+﻿namespace TreeDataType
+{
+    public class TreeNode
+    {
+        public int Value { get; set; }
+        public TreeNode Left { get; set; }
+        public TreeNode Right { get; set; }
+
+        public TreeNode(int value, TreeNode left, TreeNode right)
+        {
+            Value = value;
+            Left = left;
+            Right = right;
+        }
+    }
+}
